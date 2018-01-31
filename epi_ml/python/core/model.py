c5e7cbba5dda451a76c91e2b18cddafb2843c845
laperlej <jonathan.laperle@usherbrooke.ca>
2018-01-31 09:12:15 -0500

    fully modular

--- epi_ml/python/core/model.py
+++ epi_ml/python/core/model.py
@@ -23,6 +23,7 @@ class Model(ABC):
         self._loss = None
         self._optimizer = None
         self._predictor = None
+        self._preprocess = lambda x: x
 
     @property
     def x(self):
@@ -68,31 +69,46 @@ class Model(ABC):
     def predictor(self):
         return self._predictor
 
+    @property
+    def preprocess(self):
+        return self._preprocess
+
 
 class Cnn(Model):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, shape):
         super().__init__()
         self._x_size = input_size
         self._y_size = output_size
+        self._shape = shape
         self._x = tf.placeholder(tf.float32, [None, self._x_size])
         self._y = tf.placeholder(tf.float32, [None, self._y_size])
         self._model = self._init_model()
         self._loss = self._init_loss()
         self._optimizer = self._init_optimizer()
         self._predictor = self._init_predictor()
+        self._preprocess = self._init_preprocess()
 
     def _init_model(self):
-        input_layer = tf.reshape(self._x, [-1, 129, 5, 1])
-        conv1 = tf.layers.conv2d(inputs=input_layer, filters=32, kernel_size=[5, 5], padding="same", activation=tf.nn.relu)
-        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-        conv2 = tf.layers.conv2d(inputs=pool1, filters=64, kernel_size=[5, 5], padding="same", activation=tf.nn.relu)
-        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-        pool2_flat = tf.reshape(pool2, [-1, 32 * 1 * 64])
-        hl_units = 1024
+        conv1_size = [5, 5]
+        conv1_filters = 32
+        pool1_size = [2, 2]
+
+        conv2_size = [5, 5]
+        conv2_filters = 64
+        pool2_size = [2, 2]
+
+        input_layer = tf.reshape(self._x, [-1, self._shape[0], self._shape[1], 1])
+        conv1 = tf.layers.conv2d(inputs=input_layer, filters=conv1_filters, kernel_size=conv1_size, padding="same", activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=pool1_size, strides=2)
+        conv2 = tf.layers.conv2d(inputs=pool1, filters=conv2_filters, kernel_size=conv2_size, padding="same", activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=pool2_size, strides=2)
+        after_conv_size = int(self._shape[0]/(pool1_size[0]*pool2_size[0])) * int(self._shape[1]/(pool1_size[1]*pool2_size[1])) * conv2_filters
+        pool2_flat = tf.reshape(pool2, [-1, after_conv_size])
+        hl_units = int((after_conv_size + self._y_size)/2)
         dense = tf.layers.dense(inputs=pool2_flat, units=hl_units, activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
         dropout = tf.nn.dropout(dense, self._keep_prob)
         with tf.name_scope('Model'):
-            model = tf.layers.dense(inputs=self._model.output, units=self._model.size_y, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._hyperparams.get("beta")))
+            model = tf.layers.dense(inputs=dropout, units=self._y_size, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
         return model
 
     def _init_loss(self):
@@ -110,31 +126,50 @@ class Cnn(Model):
             predictor = tf.nn.softmax(self._model)
         return predictor
 
+    def _init_preprocess(self):
+        return lambda x: np.reshape(signal.spectrogram(x, window=("gaussian",.005), noverlap=40, nfft=80, nperseg=64, fs=16000)[2], self._x_size)
+
 
 class BidirectionalRnn(Model):
     def __init__(self, input_size, output_size):
         super().__init__()
         self._x_size = input_size
         self._y_size = output_size
-        self._x = tf.placeholder(tf.float32, [None, 5, self._x_size/5])
-        self._y = tf.placeholder(tf.float32, [None, output_size])
+        self._x = tf.placeholder(tf.float32, [None, self._x_size])
+        self._y = tf.placeholder(tf.float32, [None, self._y_size])
         self._model = self._init_model()
         self._loss = self._init_loss()
         self._optimizer = self._init_optimizer()
         self._predictor = self._init_predictor()
 
     def _init_model(self):
-        input_layer = tf.unstack(self._x, 5, 1)
+        seq_len = 25
         hl_units = int((self._x_size + self._y_size)/2)*2
-        lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(int(hl_units/2), forget_bias=1.0)
-        lstm_fw_dropout = tf.contrib.rnn.DropoutWrapper(lstm_fw_cell, self._keep_prob)
-        lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(int(hl_units/2), forget_bias=1.0)
-        lstm_bw_dropout = tf.contrib.rnn.DropoutWrapper(lstm_bw_cell, self._keep_prob)
-        outputs, a, b = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_dropout, lstm_bw_dropout, input_layer, dtype=tf.float32)
-        dense = tf.layers.dense(inputs=outputs[-1], units=hl_units, activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
+        nb_layers = 1
+
+        #tf_b_VCCs_AMs_BN1 = tf.layers.batch_normalization(tf_b_VCCs_AMs, # the input vector, size [#batches, #time_steps, 2]
+        #axis=-1, # axis that should be normalized 
+        #training=Flg_training, # Flg_training = True during training, and False during test
+        #trainable=True,
+        #name="Inputs_BN"
+        #)
+
+        sequences = tf.reshape(self._x, (-1, seq_len, int(self._x_size/seq_len)))
+        input_layer = tf.unstack(sequences, seq_len, 1)
+
+        layers=[input_layer]
+        for i in range(nb_layers):
+            lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(int(hl_units/2), forget_bias=1.0)
+            lstm_fw_dropout = tf.contrib.rnn.DropoutWrapper(lstm_fw_cell, self._keep_prob)
+            lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(int(hl_units/2), forget_bias=1.0)
+            lstm_bw_dropout = tf.contrib.rnn.DropoutWrapper(lstm_bw_cell, self._keep_prob)
+            outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_dropout, lstm_bw_dropout, layers[i], dtype=tf.float32)
+            layers.append(outputs[-1])
+
+        dense = tf.layers.dense(inputs=layers[-1], units=hl_units, activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
         dropout = tf.nn.dropout(dense, self._keep_prob)
         with tf.name_scope('Model'):
-            model = tf.layers.dense(inputs=self._model.output, units=self._model.size_y, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._hyperparams.get("beta")))
+            model = tf.layers.dense(inputs=dropout, units=self._y_size, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
         return model
 
     def _init_loss(self):
@@ -166,11 +201,15 @@ class Dense(Model):
         self._predictor = self._init_predictor()
 
     def _init_model(self):
-        hl_units = int((self._x_size + self._y_size)/2)
-        dense = tf.layers.dense(inputs=self._x, units=hl_units, activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
-        dropout = tf.nn.dropout(dense, self.keep_prob)
+        hl_units = int((self._x_size + self._y_size))
+        layers = [self._x]
+        nb_layers=3
+        for i in range(nb_layers):
+            dense = tf.layers.dense(inputs=layers[i], units=hl_units, activation=tf.nn.relu, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
+            dropout = tf.nn.dropout(dense, self.keep_prob)
+            layers.append(dropout)
         with tf.name_scope('Model'):
-            model = tf.layers.dense(inputs=dropout, units=self._y_size, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
+            model = tf.layers.dense(inputs=layers[-1], units=self._y_size, kernel_regularizer= tf.contrib.layers.l2_regularizer(scale=self._beta))
         return model
 
     def _init_loss(self):
