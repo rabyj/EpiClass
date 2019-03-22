fc0737a6d9cc588e988bae4dc271b784c4432645
Joanny Raby <Joanny.Raby@USherbrooke.ca>
2019-03-22 14:02:32 -0400

    DataSource.py: Separate handling of external files

--- epi_ml/python/core/data.py
+++ epi_ml/python/core/data.py
@@ -9,24 +9,7 @@ import collections
 import math
 import io
 
-class EpiDataSource(object):
-    """used to load metadata"""
-    def __init__(self, hdf5: io.IOBase, chromsize: io.IOBase, metadata: io.IOBase):
-        self._hdf5 = hdf5
-        self._chromsize = chromsize
-        self._metadata = metadata
-
-    @property
-    def hdf5_file(self) -> io.IOBase:
-        return self._hdf5
-
-    @property
-    def chromsize_file(self) -> io.IOBase:
-        return self._chromsize
-
-    @property
-    def metadata_file(self) -> io.IOBase:
-        return self._metadata
+from dataSource import EpiDataSource
 
 class EpiData(object):
     """used to load and preprocess epigenomic data"""
@@ -282,3 +265,4 @@ class Data(object):
     @property
     def num_examples(self):
         return self._num_examples
+
