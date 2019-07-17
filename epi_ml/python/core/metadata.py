89e59f9ecc3e7665b63f5ad01562359a46f5ba57
Joanny Raby <Joanny.Raby@USherbrooke.ca>
2019-07-17 17:58:33 -0400

    Handle healthy category with external file and class

--- epi_ml/python/core/metadata.py
+++ epi_ml/python/core/metadata.py
@@ -1,6 +1,7 @@
 import json
 import collections
 import io
+import os.path
 
 from data_source import EpiDataSource
 
@@ -88,88 +89,17 @@ class Metadata(object):
         print('For a total of {} examples\n'.format(i))
 
     def create_healthy_category(self):
-        """Combine "disease" and "donor_health_status" to create a "healthy" category."""
+        """Combine "disease" and "donor_health_status" to create a "healthy" category.
 
-        healthy_pairs = {
-            ("apparently_healthy", "apparently_healthy"),
-            ("healthy", "na"),
-            ("healthy", "healthy"),
-            ("healthy", "healty"),
-            ("healthy", "_"),
-            ("healthy", "."),
-            ("healthy", None),
-            ("healthy", ""),
-            ("no_ad_evident_at_demise", "no_ad_evident_at_demise"),
-            ("no_ad_present", "no_ad_present"),
-            ("none", "na"),
-            ("none", None),
-            ("none", ""),
-            ("none", "normal"),
-            ("none", "disease_free"),
-            ("none", "healthy,_no_prior_medical_history_(no_diabetes,_hypertension,_coronary_artery_disease,_cancer)"),
-            ("none", "normal,_renal_cell_ca_(clear_cell_type)"),
-            ("none", "normal_smooth_muscle"),
-            ("none", "non_localized_disease_free"),
-            ("none", "normal_mucosa"),
-            ("none", "polysubstance_abuse_(no_diabetes,_hypertension,_coronary_artery_disease,_cancer)"),
-            ("none", "none"),
-            ("none", "healthy"),
-            ("normal", "normal"),
-            ("negative_for_bac/fung/myc,_negative_for_hiv_1/hbv/hcv", "negative_for_bac/fung/myc,_negative_for_hiv_1/hbv/hcv"),
-            ("obesity", "healthy"),
-            ("presumed_normal", "presumed_normal"),
-            ("presumed_normal", None),
-            (None, "healthy"),
-            ("", "healthy"),
-            (None, "presumed_healthy"),
-            ("", "presumed_healthy")
-        }
-
-        ignore_pairs = {
-            ("na", "na"),
-            ("none", "unknown"),
-            ("unknown", "unknown"),
-            ("unknown", "na"),
-            ("autism_spectrum_disorder", "na"),
-            ("disease_type_and/or_category_unknown", "na"),
-            (None, "probable_tonsillitis"),
-            ("", "probable_tonsillitis"),
-            (None, "na"),
-            ("", "na"),
-            (None, None),
-            ("", "")
-        }
-
-        # self._test_healthy_hg38()
-
-        for dataset in self.datasets:
-            disease = dataset.get("disease", None)
-            donor_health_status = dataset.get("donor_health_status", None)
-
-            if (disease, donor_health_status) in healthy_pairs:
-                dataset["healthy"] = "y"
-            elif (disease, donor_health_status) in ignore_pairs:
-                continue
-            else:
-                dataset["healthy"] = "n"
-
-        # self._test_healthy_hg38()
-
-    def _test_healthy_hg38(self):
-
-        test_md5s = ["379bed415f8e3fb17456115e68e5e773", "4749dbc09f7af9731f22cb1818dcefbd", "64b754ec8bc10b1f45b9990c387dda1f"]
-        """ disease:donor_health_status
-        "":""
-        "":"na"
-        "none":""
+        When a dataset has pairs with unknow correspondance, it does not add
+        the category, and so these datasets are ignored through remove_missing_labels().
         """
-
-        for md5 in test_md5s:
-            dataset = self._metadata[md5]
-            disease = dataset.get("disease", "absent")
-            donor_health_status = dataset.get("donor_health_status", "absent")
-            healthy = dataset.get("healthy", "absent")
-            print("md5:{}\ndisease: {}\ndonor_health_status: {}\nhealthy:{}\n".format(md5, disease, donor_health_status, healthy))
+        healthy_category = HealthyCategory()
+        for dataset in self.datasets:
+            healthy = healthy_category.get_healthy_status(dataset)
+            if healthy == "?":
+                continue
+            dataset["healthy"] = healthy
 
     def merge_molecule_classes(self):
         """Combine similar classes pairs in the molecule category."""
@@ -179,3 +109,45 @@ class Metadata(object):
                 dataset["molecule"] = "total_rna"
             elif molecule == "polyadenylated_mrna":
                 dataset["molecule"] = "polya_rna"
+
+
+class HealthyCategory(object):
+    """Create/Represent/manipulate the "healthy" metadata category"""
+    def __init__(self):
+        self.pairs_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "healthy_category.tsv"
+            )
+        self.healthy_dict = self.read_healthy_pairs()
+
+    @staticmethod
+    def get_healthy_pairs(datasets):
+        """Return set of (disease, donor_health_status) pairs."""
+        pairs = set([])
+        for dataset in datasets:
+            disease = dataset.get("disease", "--empty--")
+            donor_health_status = dataset.get("donor_health_status", "--empty--")
+            pairs.add((disease, donor_health_status))
+        return pairs
+
+    def list_healthy_pairs(self, datasets):
+        """List unique (disease, donor_health_status) pairs."""
+        for pair in sorted(self.get_healthy_pairs(datasets)):
+            print("{}\t{}".format(*pair))
+
+    def read_healthy_pairs(self):
+        """Return a (disease, donor_health_status):healthy dict defined in
+        a tsv file with disease|donor_health_status|healthy columns.
+        """
+        healthy_dict = {}
+        with open(self.pairs_file, "r") as tsv_file:
+            next(tsv_file) # skip header
+            for line in tsv_file:
+                disease, donor_health_status, healthy = line.rstrip('\n').split('\t')
+                healthy_dict[(disease, donor_health_status)] = healthy
+        return healthy_dict
+
+    def get_healthy_status(self, dataset):
+        """Return "y", "n" or "?" depending of the healthy status of the dataset."""
+        disease = dataset.get("disease", "--empty--")
+        donor_health_status = dataset.get("donor_health_status", "--empty--")
+        return self.healthy_dict[(disease, donor_health_status)]
