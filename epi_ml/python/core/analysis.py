rabyj/EpiClass

commit db248b4f390f1829ee11477c0ba8cba5f22d6e9f

Rename to_bedgraph fct + formatting cleanup

epi_ml/python/core/analysis.py
@@ -1,4 +1,3 @@
-import io
 import itertools
 import os
 
@@ -58,7 +57,7 @@ class ConfusionMatrix(object):
     def __init__(self, labels, tf_confusion_mat):
         self._labels = labels
         self._confusion_matrix = self._create_confusion_matrix(tf_confusion_mat) #pd dataframe
-        
+
     @classmethod
     def from_csv(cls, csv_path):
         obj = cls.__new__(cls)  # Does not call __init__
@@ -74,24 +73,24 @@ class ConfusionMatrix(object):
         return pd.DataFrame(data=confusion_mat, index=labels_w_count, columns=self._labels)
 
     def _to_relative_confusion_matrix(self, labels_count, tf_confusion_mat):
-        confusion_mat = tf_confusion_mat/labels_count 
+        confusion_mat = tf_confusion_mat/labels_count
         confusion_mat = np.nan_to_num(confusion_mat)
         confusion_mat = confusion_mat.T #one label per row instead of column
         return confusion_mat
 
     def to_png(self, path):
         plt.figure()
-        
+
         data_mask = np.ma.masked_where(self._confusion_matrix == 0, self._confusion_matrix)
 
-        cdict = {'red':   ((0.0,  0.1, 0.1),
-                           (1.0,  1.0, 1.0)),
+        cdict = {'red': ((0.0, 0.1, 0.1),
+                         (1.0, 1.0, 1.0)),
 
-                'green': ((0.0,  0.1, 0.1),
-                          (1.0,  0.1, 0.1)),
+                 'green': ((0.0, 0.1, 0.1),
+                           (1.0, 0.1, 0.1)),
 
-                'blue': ((0.0,  1.0, 1.0),
-                         (1.0,  0.1, 0.1))}
+                 'blue': ((0.0, 1.0, 1.0),
+                          (1.0, 0.1, 0.1))}
 
         blue_red = matplotlib.colors.LinearSegmentedColormap('BlueRed', cdict, N=1000)
 
@@ -149,8 +148,8 @@ def write_pred_table(pred, classes, data_subset, path):
     string_labels = [classes[np.argmax(label)] for label in labels]
     df = pd.DataFrame(data=pred, index=md5s, columns=classes)
 
-    df.insert(loc=0 , column="class", value=string_labels)
-    
+    df.insert(loc=0, column="class", value=string_labels)
+
     df.to_csv(path, encoding="utf8")
 
 def convert_matrix_csv_to_png(in_path, out_path):
@@ -173,7 +172,7 @@ def metrics(acc, pred, data_subset):
     print("{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(acc, precision, recall, f1, mcc))
 
 def importance(w):
-    #garson algorithm, w for weights 
+    #garson algorithm, w for weights
     #TODO: generalise, put in model
     total_w = w[0]
     for i in range(2, len(w), 2):
@@ -193,7 +192,7 @@ def predict_concat_size(chroms, resolution):
         if size_of_mean%resolution == 0:
             concat_size += size_of_mean
         else:
-             concat_size += size_of_mean + 1
+            concat_size += size_of_mean + 1
 
     return concat_size
 
@@ -210,18 +209,16 @@ def assert_correct_resolution(chroms, resolution, signal_length):
     if predict_concat_size(chroms, resolution) != signal_length:
         raise AssertionError("Signal_length not coherent with given resolution of {}.".format(resolution))
 
-def bedgraph_from_importance(importance, chroms, resolution, bedgraph_path):
-    """Write a bedgraph from the computed importance of features.
+def values_to_bedgraph(values, chroms, resolution, bedgraph_path):
+    """Write a bedgraph from a full genome values iterable (e.g. importance).
     The chromosome coordinates are zero-based, half-open (from 0 to N-1).
     """
-    importance_index = 0
     with open(bedgraph_path, 'w') as my_bedgraph:
         for name, size in chroms:
 
             positions = itertools.chain(range(0, size, resolution), [size-1])
 
-            for pos1, pos2 in pairwise(positions):
+            for i, (pos1, pos2) in enumerate(pairwise(positions)):
 
-                line = [name, pos1, pos2, importance[importance_index]]
+                line = [name, pos1, pos2, values[i]]
                 my_bedgraph.write("{}\t{}\t{}\t{}\n".format(*line))
-                importance_index += 1

epi_ml/python/main.py
@@ -7,8 +7,6 @@ import sys
 import warnings
 warnings.simplefilter("ignore")
 
-import numpy as np
-
 from argparseutils.directorytype import DirectoryType
 from core import metadata
 from core import data
@@ -30,13 +28,12 @@ def parse_arguments(args: list) -> argparse.Namespace:
     arg_parser.add_argument('logdir', type=DirectoryType(), help='A directory for the logs.')
     return arg_parser.parse_args(args)
 
-# @profile
 def main(args):
     """main called from command line, edit to change behavior"""
     begin = datetime.datetime.now()
     print("begin {}".format(begin))
 
-    # parse params
+    # --- parse params ---
     epiml_options = parse_arguments(args)
 
     # if only want to convert confusion matrix csv to png
@@ -45,59 +42,78 @@ def main(args):
     # analysis.convert_matrix_csv_to_png(in_path, out_path)
     # sys.exit()
 
-    # load external files
+    # --- load external files ---
     my_datasource = data.EpiDataSource(
         epiml_options.hdf5,
         epiml_options.chromsize,
         epiml_options.metadata
         )
 
-    # load useful info
-    hdf5_resolution = my_datasource.hdf5_resolution()
-    chroms = my_datasource.load_chrom_sizes()
+    # --- load useful info ---
+    # hdf5_resolution = my_datasource.hdf5_resolution()
+    # chroms = my_datasource.load_chrom_sizes()
 
-    # load data
+    # --- load data ---
     my_metadata = metadata.Metadata.from_epidatasource(my_datasource)
-    # my_metadata = metadata.keep_major_cell_types(my_metadata)
-    # my_metadata.select_category_subset(os.getenv("STEP1_ASSAY"), "assay")
+
+    # --- Categories creation/change ---
     # my_metadata.create_healthy_category()
     # my_metadata.merge_molecule_classes()
+    # my_metadata.merge_fetal_tissues()
 
+    # --- Dataset selection ---
+
+    my_metadata = metadata.keep_major_cell_types(my_metadata)
+    # my_metadata = metadata.keep_major_cell_types_alt(my_metadata)
+    # my_metadata.remove_category_subsets([os.getenv("REMOVE_ASSAY", "")], "assay")
+    # my_metadata.select_category_subsets([os.getenv("SELECT_ASSAY", "")], "assay")
+    # my_metadata = metadata.special_case_2(my_metadata)
+
+    # my_metadata = metadata.five_cell_types_selection(my_metadata)
+    # assays_to_remove = [os.getenv(var, "") for var in ["REMOVE_ASSAY1", "REMOVE_ASSAY2", "REMOVE_ASSAY3"]]
+    # my_metadata.remove_category_subsets(assays_to_remove, "assay")
+
+    # --- Create training/validation/test sets (and change metadata according to what is used) ---
     my_data = data.DataSetFactory.from_epidata(
         my_datasource, my_metadata, epiml_options.category, oversample=True, min_class_size=10
         )
-    my_metadata.display_labels(epiml_options.category)
+    # my_metadata.display_labels(epiml_options.category)
+    my_metadata.display_labels("cell_type")
+    my_metadata.display_labels("assay")
 
-    # define sizes for input and output layers of the network
+    # --- define sizes for input and output layers of the network ---
     input_size = my_data.train.signals[0].size
     ouput_size = my_data.train.labels[0].size
 
-    # Assert the resolution is correct so the importance bedgraph works later
-    analysis.assert_correct_resolution(chroms, hdf5_resolution, input_size)
+    # --- Assert the resolution is correct so the importance bedgraph works later ---
+    # analysis.assert_correct_resolution(chroms, hdf5_resolution, input_size)
 
-    # choose a model
+    # --- choose a model ---
     my_model = model.Dense(input_size, ouput_size)
     #my_model = model.Cnn(41*49, ouput_size, (41, 49))
     #my_model = model.BidirectionalRnn(input_size, ouput_size)
 
-    # trainer for the model
+    # --- trainer for the model ---
     hparams = json.load(epiml_options.hyperparameters)
     my_trainer = trainer.Trainer(my_data, my_model, epiml_options.logdir, **hparams)
 
-    # train the model
+    # --- train the model ---
     before_train = datetime.datetime.now()
     my_trainer.train()
     print("training time: {}".format(datetime.datetime.now() - before_train))
 
-    # outputs
+    # --- restore old model ---
+    # my_trainer.restore()
+
+    # --- outputs ---
     my_analyzer = analysis.Analysis(my_trainer)
 
-    # Print metrics
+    # --- Print metrics ---
     my_analyzer.training_metrics()
     my_analyzer.validation_metrics()
     # my_analyzer.test_metrics()
 
-    # Create prediction file
+    # --- Create prediction file ---
     # outpath1 = os.path.join(epiml_options.logdir, "training_predict.csv")
     outpath2 = os.path.join(epiml_options.logdir, "validation_predict.csv")
     # outpath3 = os.path.join(epiml_options.logdir, "test_predict.csv")
@@ -106,21 +122,24 @@ def main(args):
     my_analyzer.validation_prediction(outpath2)
     # my_analyzer.test_prediction(outpath3)
 
-    # Create confusion matrix
+    # --- Create confusion matrix ---
+    # my_analyzer.training_confusion_matrix(epiml_options.logdir)
     my_analyzer.validation_confusion_matrix(epiml_options.logdir)
     # my_analyzer.test_confusion_matrix(epiml_options.logdir)
 
-    # Create visualisation
+    # --- Create visualisation ---
 
     # vis = visualization.Pca()
     # my_trainer.visualize(vis)
 
+    # --- Compute/write importance ---
+
     # importance = pickle.load(open("importance.pickle", 'rb'))
-    importance = my_analyzer.importance() #TODO: generalize, probably put in model
+    # importance = my_analyzer.importance() #TODO: generalize, probably put in model
     # pickle.dump(importance, open("importance.pickle", 'wb'))
 
-    bedgraph_path = os.path.join(epiml_options.logdir, "importance.bedgraph")
-    analysis.bedgraph_from_importance(importance, chroms, hdf5_resolution, bedgraph_path)
+    # bedgraph_path = os.path.join(epiml_options.logdir, "importance.bedgraph")
+    # analysis.values_to_bedgraph(importance, chroms, hdf5_resolution, bedgraph_path)
 
     end = datetime.datetime.now()
     print("end {}".format(end))

commit e86a2a99e7dca7bf7df691932008b8c2ab4db33e

Undo bug introduced in to_bedgraph in commit db248b4

epi_ml/python/core/analysis.py
@@ -213,12 +213,14 @@ def values_to_bedgraph(values, chroms, resolution, bedgraph_path):
     """Write a bedgraph from a full genome values iterable (e.g. importance).
     The chromosome coordinates are zero-based, half-open (from 0 to N-1).
     """
+    i = 0
     with open(bedgraph_path, 'w') as my_bedgraph:
         for name, size in chroms:
 
             positions = itertools.chain(range(0, size, resolution), [size-1])
 
-            for i, (pos1, pos2) in enumerate(pairwise(positions)):
+            for pos1, pos2 in enumerate(pairwise(positions)):
 
                 line = [name, pos1, pos2, values[i]]
                 my_bedgraph.write("{}\t{}\t{}\t{}\n".format(*line))
+                i += 1