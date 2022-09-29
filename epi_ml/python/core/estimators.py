rabyj/EpiClass

commit ab654d2abed79984094aeec23769947add03bbe9

Get full pred values when possible.

epi_ml/python/core/estimators.py
@@ -120,10 +120,16 @@ class EstimatorAnalyzer(object):
     def predict_file(self, ids, X, y, log):
         """Write predictions table for validation set."""
 
-        int_results = self._clf.predict(X)
-        pred_results = self.encoder.transform(int_results)
+        try:
+            pred_results = self._clf.predict_proba(X)
+        except AttributeError:
+            int_results = self._clf.predict(X)
+            pred_results = self.encoder.transform(int_results)
 
-        str_preds = [self.mapping[encoded_label] for encoded_label in int_results]
+        str_preds = [
+            self.mapping[encoded_label] for encoded_label in np.argmax(pred_results, axis=1)  # type: ignore
+        ]
+
         str_y = [self.mapping[encoded_label] for encoded_label in y]
 
         write_pred_table(

commit c7a6759f76d723060d5ce14bfba8e16886dc23e0

Fix 2 class edge case bug introduced in ab654d2abed79984094aeec23769947add03bbe9

epi_ml/python/core/estimators.py
@@ -145,6 +145,8 @@ class EstimatorAnalyzer(object):
         except AttributeError:
             int_results = self._clf.predict(X)
             pred_results = self.encoder.transform(int_results)
+            if pred_results.shape[1] == 1:  # 2 classes
+                pred_results = [[1, 0] if i == 0 else [0, 1] for i in int_results]
 
         str_preds = [
             self.mapping[encoded_label] for encoded_label in np.argmax(pred_results, axis=1)  # type: ignore