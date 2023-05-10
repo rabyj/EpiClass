rabyj/EpiClass

commit bda142979a8291a58495ba55330408d4da66bb6c

Major. pytorch model now outputs logits.
- NN outputs logits, was needed to make shap explainer work.
- SHAP computation parallelized.

src/python/epi_ml/core/model_pytorch.py
@@ -1,4 +1,5 @@
 """Model creation module"""
+# pylint: disable=unused-argument, arguments-differ
 # pyright: reportPrivateImportUsage=false
 from __future__ import annotations
 
@@ -8,6 +9,7 @@ from typing import Dict
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
+from numpy.typing import ArrayLike
 from torch import Tensor, nn
 from torch.utils.data import TensorDataset
 from torchinfo import summary
@@ -93,7 +95,8 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
         for _ in range(0, self._nb_layer - 1):
             layer_list.append(nn.Linear(self._hl_size, self._hl_size))
             layer_list.append(nn.ReLU())
-            # in case of ReLU, dropout should be applied before for computational efficiency, swapping them gives same result
+            # in case of ReLU, dropout should be applied before for computational efficiency,
+            # swapping them gives same result
             # https://sebastianraschka.com/faq/docs/dropout-activation.html
 
         # output layer
@@ -113,20 +116,32 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
 
     # --- Define format of output ---
     def forward(self, x: torch.Tensor):
-        """Return probabilities."""
-        return F.softmax(self.forward_train(x), dim=1)
-
-    def forward_train(self, x):
         """Return logits."""
         return self._pt_model(x)
+
+    def predict_proba(self, x: torch.Tensor) -> ArrayLike:
+        """Return probabilities"""
+        self.eval()
+        with torch.no_grad():
+            logits = self(x)
+            probs = F.softmax(logits, dim=1).numpy()
+        return probs
+
+    def predict_class(self, x: torch.Tensor) -> ArrayLike:
+        """Return class"""
+        self.eval()
+        with torch.no_grad():
+            logits = self(x)
+            preds = torch.argmax(logits, dim=1).numpy()
+        return preds
 
     # --- Define how training and validation is done, what loss is used ---
     def training_step(self, train_batch, batch_idx):
         """Return training loss and co."""
         x, y = train_batch
-        logits = self.forward_train(x)
+        logits = self(x)
         loss = F.cross_entropy(logits, y)
-        preds = F.softmax(logits, dim=1)
+        preds = torch.argmax(logits, dim=1)
         return {"loss": loss, "preds": preds.detach(), "target": y}
 
     def training_step_end(self, outputs):
@@ -144,10 +159,10 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
     def validation_step(self, val_batch, batch_idx):
         """Return validation loss and co."""
         x, y = val_batch
-        logits = self.forward_train(x)
+        logits = self(x)
         loss = F.cross_entropy(logits, y)
-        preds = F.softmax(logits, dim=1)
-        return {"loss": loss, "preds": preds, "target": y}
+        preds = torch.argmax(logits, dim=1)
+        return {"loss": loss, "preds": preds.detach(), "target": y}
 
     def validation_step_end(self, outputs):
         """Update and log validation metrics."""
@@ -179,20 +194,20 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
             preds = self(features)
         return self.metrics(preds, targets)
 
-    def compute_predictions_from_dataset(self, dataset: TensorDataset):
-        """Return predictions and targets from dataset."""
+    def compute_predictions_from_dataset(self, dataset: TensorDataset) -> ArrayLike:
+        """Return probability predictions and targets from dataset."""
         self.eval()
         with torch.no_grad():
             features, targets = dataset[:]
-            preds = self(features)
-        return preds, targets
+            probs = self.predict_proba(features)
+        return probs, targets
 
-    def compute_predictions_from_features(self, features: Tensor):
-        """Return predictions from features."""
+    def compute_predictions_from_features(self, features: Tensor) -> ArrayLike:
+        """Return probability predictions from features."""
         self.eval()
         with torch.no_grad():
-            preds = self(features)
-        return preds
+            probs = self.predict_proba(features)
+        return probs
 
     @classmethod
     def restore_model(cls, model_dir):

.gitignore
@@ -1,5 +1,5 @@
 # project specific
-saccer3/model
+**/saccer3/model/**
 
 # cluster files
 slurm_files/

src/python/epi_ml/compute_shaps.py
@@ -43,7 +43,7 @@ def parse_arguments() -> argparse.Namespace:
         "model", type=DirectoryChecker(), help="Directory where to load the classifier to explain.",
     )
     arg_parser.add_argument(
-        "-o", "--output_name", metavar="--output-name", type=str, help="Name (not path) of outputed pickle file containing computed SHAP values", # pylint: disable=line-too-long
+        "-o", "--output_name", metavar="--output-name", help="Name (not path) of outputed pickle file containing computed SHAP values", # pylint: disable=line-too-long
     )
     # fmt: on
     return arg_parser.parse_args()
@@ -70,7 +70,7 @@ def benchmark(metadata: Metadata, datasource: EpiDataSource, model):
 
     for n in [250]:
         train_data = full_data.train.subsample(list(range(n)))
-        shap_computer = SHAP_Handler(model=model, logdir=None)
+        shap_computer = SHAP_Handler(model=model, logdir="")
 
         eval_size = 25
         evaluation_data = full_data.train.subsample(list(range(n, n + eval_size)))

src/python/epi_ml/core/shap_values.py
@@ -1,9 +1,10 @@
 """Module containing shap values related code (e.g. handling computation, analysing results)."""
 from __future__ import annotations
 
-import pickle
+import concurrent.futures
+import copy
 from pathlib import Path
-from typing import Any, List, Tuple
+from typing import List, Tuple
 
 import matplotlib
 
@@ -11,12 +12,14 @@ matplotlib.use("Agg")
 import numpy as np
 import pandas as pd
 import shap
-from numpy.typing import ArrayLike
-from torch import Tensor
+import torch
+import torch.nn.functional as F
 
 from epi_ml.core.model_pytorch import LightningDenseClassifier
 from epi_ml.core.types import SomeData
 from epi_ml.utils.time import time_now_str
+
+# from numpy.typing import ArrayLike
 
 
 class SHAP_Handler:
@@ -24,101 +27,95 @@ class SHAP_Handler:
 
     def __init__(self, model: LightningDenseClassifier, logdir: Path | str):
         self.model = model
+        self.model.eval()
+        self.model_classes = list(self.model.mapping.items())
         self.logdir = logdir
         self.filename_template = "shap_{name}_{time}.{ext}"
 
     def _create_filename(self, ext: str, name="") -> Path:
+        """Create a filename with the given extension and name, and a timestamp."""
         filename = self.filename_template.format(name=name, ext=ext, time=time_now_str())
-
-        return Path(self.logdir) / filename
+        filename = Path(self.logdir) / filename
+        return filename
 
     def compute_NN(
-        self,
-        background_dset: SomeData,
-        evaluation_dset: SomeData,
-        save=True,
-        name="",
-        load_filepath: Path | str | None = None,
+        self, background_dset: SomeData, evaluation_dset: SomeData, save=True, name=""
     ) -> Tuple[shap.DeepExplainer, List[np.ndarray]]:
         """Compute shap values of deep learning model on evaluation dataset
         by creating an explainer with background dataset.
 
-        Loads explainer from given filepath if given.
-
         Returns explainer and shap values (as a list of matrix per class)
         """
-        if load_filepath is not None:
-            explainer = self.load_from_pickle(load_filepath)
-        else:
-            explainer = shap.DeepExplainer(
-                model=self.model, data=Tensor(background_dset.signals)
+        explainer = shap.DeepExplainer(
+            model=self.model, data=torch.from_numpy(background_dset.signals).float()
+        )
+        if save:
+            np.savez_compressed(
+                file=self._create_filename(
+                    name=name + "_explainer_background", ext="npz"
+                ),
+                background_md5s=background_dset.ids,
+                background_expectation=explainer.expected_value,  # type: ignore
+                classes=self.model_classes,
             )
 
-        if save and load_filepath is None:
-            self.save_explainer(explainer, "explainer_" + name)
-
-        shap_values = explainer.shap_values(Tensor(evaluation_dset.signals))
+        signals = torch.from_numpy(evaluation_dset.signals).float()
+        shap_values = self.compute_shap_values_parallel(explainer, signals)
 
         if save:
-            self.save_to_pickle(shap_values, evaluation_dset.ids, name, explainer)
+            np.savez_compressed(
+                file=self._create_filename(name=name + "_evaluation", ext="npz"),
+                evaluation_md5s=evaluation_dset.ids,
+                shap_values=shap_values,
+                classes=self.model_classes,
+            )
 
         return explainer, shap_values  # type: ignore
 
-    def save_explainer(self, explainer: shap.Explainer, name: str = "") -> Path:
-        """Save explainer to pickle file."""
-        filename = self._create_filename(name=name, ext="pickle")
-
-        print(f"Saving explainer to: {filename}")
-        with open(filename, "wb") as f:
-            pickle.dump(explainer, f)
-
-        return filename
-
-    def save_to_pickle(
-        self,
-        shap_values: ArrayLike,
-        ids: List[str],
-        name: str = "",
-        explainer: shap.Explainer | None = None,
-    ) -> Path:
-        """Save shap values with assigned signals ids to a pickle file.
-        Also save the explainer if given.
-
-        Returns path of saved file."""
-
-        filename = self._create_filename(name=name, ext="pickle")
-
-        print(f"Saving SHAP values and {explainer} to: {filename}")
-        with open(filename, "wb") as f:
-            pickle.dump({"shap": shap_values, "ids": ids, "explainer": explainer}, f)
-
-        return filename
-
     @staticmethod
-    def load_from_pickle(path: str | Path) -> Any:
-        """Load pickle file.
+    def compute_shap_values_parallel(
+        explainer: shap.DeepExplainer,
+        signals: torch.Tensor,
+        num_workers: int = 4,
+    ) -> List[np.ndarray]:
+        """Compute SHAP values in parallel using a ThreadPoolExecutor.
 
-        Returns {"shap": shap_values, "ids": ids, "explainer": shap_explainer} dict
-        if given filepath of a saved shap values file.
+        Args:
+            explainer (shap.DeepExplainer): The SHAP explainer object used for computing SHAP values.
+            signals (torch.Tensor): The evaluation dataset samples as a torch Tensor of shape (#samples, #features).
+            num_workers (int, optional): The number of parallel threads to use for computation. Defaults to 4.
+
+        Returns:
+            List[np.ndarray]: A list of SHAP values matrices (one per output class) of shape (#samples, #features).
         """
-        with open(path, "rb") as f:
-            data = pickle.load(f)
+        signal_chunks = torch.tensor_split(signals, num_workers)
 
-        return data
+        def worker(chunk: torch.Tensor) -> np.ndarray:
+            explainer_copy = copy.deepcopy(explainer)
+            return explainer_copy.shap_values(chunk)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+            shap_values_chunks = list(executor.map(worker, signal_chunks))
+
+        shap_values = [
+            np.concatenate([chunk[i] for chunk in shap_values_chunks], axis=0)
+            for i in range(len(shap_values_chunks[0]))
+        ]
+
+        return shap_values
 
     def save_to_csv(
         self, shap_values_matrix: np.ndarray, ids: List[str], name: str
     ) -> Path:
         """Save a single shap value matrix (shape (n_samples, #features)) to csv.
-        Giving a name is mandatory contrary to pickle.
+        Giving a name is mandatory.
 
         Returns path of saved file.
         """
         if isinstance(shap_values_matrix, list):
             raise ValueError(
-                "{shap_values_matrix} is a list, not an array. Need one matrix of shape (n_samples, #features)"
+                f"Expected 'shap_values_matrix' to be a numpy array of shape (n_samples, #features), but got a list instead: {shap_values_matrix}"  # pylint: disable=line-too-long
             )
-
         filename = self._create_filename(name=name, ext="csv")
 
         n_dims = shap_values_matrix.shape[1]
@@ -143,12 +140,12 @@ class SHAP_Analyzer:
         explainer: The SHAP explainer object used to compute SHAP values.
     """
 
-    def __init__(self, model, explainer):
+    def __init__(self, model: LightningDenseClassifier, explainer: shap.DeepExplainer):
         self.model = model
         self.explainer = explainer
 
     def verify_shap_values_coherence(
-        self, shap_values: List, dset: SomeData, tolerance=1e-6
+        self, shap_values: List, dset: SomeData, tolerance=1e-6  # type: ignore
     ):
         """Verify the coherence of SHAP values with the model's output probabilities.
 
@@ -167,23 +164,43 @@ class SHAP_Analyzer:
         """
         num_classes = len(shap_values)
         num_samples = shap_values[0].shape[0]
-
         # Calculate the sum of SHAP values for each sample (across all classes) and add base values
         shap_sum = np.zeros((num_samples, num_classes))
         for i, shap_values_class in enumerate(shap_values):
+            # shap_values_class.sum(axis=1).shape = (n_samples,)
             shap_sum[:, i] = (
                 shap_values_class.sum(axis=1) + self.explainer.expected_value[i]
             )
 
         # Compute the model's output probabilities for the samples
-        model_output_probabilities = self.model(dset.signals).detach().numpy()
-
+        signals = torch.from_numpy(dset.signals).float()
+        model_output_logits = self.model(signals).detach()
+        probs = F.softmax(model_output_logits, dim=1).detach().numpy()
+        shap_to_prob = (
+            F.softmax(torch.from_numpy(shap_sum).float(), dim=1)
+            .sum(dim=1)
+            .detach()
+            .numpy()
+        )
+        print(
+            f"Verifying model output: Sum close to 1? {np.all(1 - probs.sum(axis=1) <= tolerance)}"
+        )
+        print(
+            f"Verifying SHAP output: Sum close to 1 (w expected value)? {np.all(1 - shap_to_prob <= tolerance)}"
+        )
+        print(f"Shap sum shape, detailling all classes: {shap_sum.shape}")
+        total = shap_sum.sum(axis=1)
+        print(
+            f"Sum of all shap values across classes, for {total.shape} samples: {total}\n"
+        )
         # Compare the sum of SHAP values with the model's output probabilities
-        diff = np.abs(shap_sum - model_output_probabilities)
+        diff = np.abs(shap_sum - model_output_logits.numpy())
         coherent = np.all(diff <= tolerance)
-
+        print(
+            f"Detailled values for shap sum, model preds and diff:\n {shap_sum}\n{model_output_logits}\n{diff}"
+        )
         if not coherent:
             problematic_samples = np.argwhere(diff > tolerance)
-            print(f"SHAP values are not coherent for samples: {problematic_samples}")
+            print(f"SHAP values are not coherent for samples:\n {problematic_samples}")
 
         return coherent

commit af047b4dc6953ef71f1fb3656cf23b1534be3010

pytorch model: stay consistent with tensor outputs
Fix bug introduced in bda142979a8291a58495ba55330408d4da66bb6c
Did not stay consistent with analysis code, torch.argmax bugged on numpy array.

src/python/epi_ml/core/model_pytorch.py
@@ -4,12 +4,11 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Tuple
 
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
-from numpy.typing import ArrayLike
 from torch import Tensor, nn
 from torch.utils.data import TensorDataset
 from torchinfo import summary
@@ -23,7 +22,8 @@ from torchmetrics import (
 )
 
 
-class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-ancestors
+# pylint: disable=too-many-ancestors
+class LightningDenseClassifier(pl.LightningModule):
     """Simple dense network handler"""
 
     def __init__(
@@ -116,24 +116,24 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
         return optimizer
 
     # --- Define format of output ---
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: Tensor):
         """Return logits."""
         return self._pt_model(x)
 
-    def predict_proba(self, x: torch.Tensor) -> ArrayLike:
+    def predict_proba(self, x: Tensor) -> Tensor:
         """Return probabilities"""
         self.eval()
         with torch.no_grad():
             logits = self(x)
-            probs = F.softmax(logits, dim=1).numpy()
+            probs = F.softmax(logits, dim=1)
         return probs
 
-    def predict_class(self, x: torch.Tensor) -> ArrayLike:
+    def predict_class(self, x: Tensor) -> Tensor:
         """Return class"""
         self.eval()
         with torch.no_grad():
             logits = self(x)
-            preds = torch.argmax(logits, dim=1).numpy()
+            preds = torch.argmax(logits, dim=1)
         return preds
 
     # --- Define how training and validation is done, what loss is used ---
@@ -150,7 +150,7 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
         preds = torch.argmax(logits, dim=1)
         return {"loss": loss, "preds": preds.detach(), "target": y}
 
-    def training_step_end(self, outputs):
+    def training_step_end(self, outputs):  # pylint: disable=arguments-renamed
         """Update and log training metrics."""
         self.train_acc(outputs["preds"], outputs["target"])
 
@@ -200,7 +200,9 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
             preds = self(features)
         return self.metrics(preds, targets)
 
-    def compute_predictions_from_dataset(self, dataset: TensorDataset) -> ArrayLike:
+    def compute_predictions_from_dataset(
+        self, dataset: TensorDataset
+    ) -> Tuple[Tensor, Tensor]:
         """Return probability predictions and targets from dataset."""
         self.eval()
         with torch.no_grad():
@@ -208,7 +210,7 @@ class LightningDenseClassifier(pl.LightningModule):  # pylint: disable=too-many-
             probs = self.predict_proba(features)
         return probs, targets
 
-    def compute_predictions_from_features(self, features: Tensor) -> ArrayLike:
+    def compute_predictions_from_features(self, features: Tensor) -> Tensor:
         """Return probability predictions from features."""
         self.eval()
         with torch.no_grad():