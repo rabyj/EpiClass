0b7f52e5b39cfa9848d485663d476bf41a38a68f
rabyj <Joanny.Raby@usherbrooke.ca>
2023-06-02 14:52:22 -0400

    clean_hdf5: efficient algorithm
    Only compute the positions to put to zero one time per hdf5 resolution.
    Massive improvement in speed.

--- src/python/tests/utils/clean_hdf5_test.py
+++ src/python/tests/utils/clean_hdf5_test.py
@@ -1,10 +1,12 @@
 """Tests for the `clean_hdf5` module."""
 from pathlib import Path
 
+import h5py
+import numpy as np
 import pytest
 
 from epi_ml.utils.clean_hdf5 import (
-    is_position_in_blacklist,
+    get_positions_to_treat,
     load_bed,
     main,
     preprocess_bed,
@@ -12,42 +14,6 @@ from epi_ml.utils.clean_hdf5 import (
 )
 
 CURRENT_DIR = Path(__file__).parent.resolve()
-
-
-def test_is_position_in_blacklist():
-    """Tests the `is_position_in_blacklist` function."""
-    # Sample blacklist bed intervals (0-based start, 1-based end coordinates)
-    blacklist_bed = [
-        ("chr1", 10, 20),
-        ("chr2", 5, 15),
-        ("chr2", 25, 30),
-        ("chr3", 10, 15),
-    ]
-
-    # Preprocess the blacklist bed
-    chromosome_intervals = preprocess_bed(blacklist_bed)
-
-    # Test cases with region overlap
-    test_cases = [
-        ("chr1", 5, 12, True),  # Region overlaps with chr1 interval
-        ("chr1", 9, 20, True),  # Region overlaps with chr1 interval
-        ("chr1", 15, 18, True),  # Region overlaps with chr1 interval
-        ("chr1", 20, 22, False),  # Region does not overlap with chr1 interval
-        ("chr2", 5, 8, True),  # Region overlaps with chr2 interval
-        ("chr2", 15, 18, False),  # Region does not overlap with chr2 interval
-        ("chr2", 14, 18, True),  # Region overlap with chr2 interval
-        ("chr2", 30, 35, False),  # Region does not overlap with chr2 interval
-        ("chr3", 10, 15, True),  # Region overlaps with chr3 interval
-        ("chr3", 12, 14, True),  # Region overlaps with chr3 interval
-        ("chr3", 5, 8, False),  # Region does not overlap with chr3 interval
-        ("chr3", 5, 8, False),  # Region does not overlap with chr3 interval
-        ("chr4", 10, 20, False),  # No intervals for chr4
-    ]
-
-    for chromosome, start, end, expected in test_cases:
-        print(f"Testing {chromosome}:{start}-{end}...")
-        result = is_position_in_blacklist(start, end, chromosome_intervals, chromosome)
-        assert result == expected, print(f"Expected {expected}, got {result}")
 
 
 @pytest.fixture(name="test_hdf5")
@@ -59,43 +25,128 @@ def test_hdf5_file_path() -> Path:
     )
 
 
-@pytest.fixture(name="test_bed")
+@pytest.fixture(name="real_test_bed")
 def test_bed_file_path() -> Path:
     """Returns the path to a test bed file."""
     return Path(CURRENT_DIR / "../fixtures/hg38_unified_blacklist.bed")
 
 
-def test_load_bed(test_bed):
+@pytest.fixture(name="mock_test_bed")
+def mock_bed_data_path(tmpdir) -> Path:
+    """Writes mock bed data to a file and returns the file path."""
+    # fmt: off
+    bed_data = [
+        ("chr1", 0, 1000),
+        ("chr1", 2000, 3000),
+        ("chr1", 4000, 5500),
+        ("chr1", 6000, 8000),
+        ("chr1", 8500, 90000),  # Spans multiple 100kb bins
+        ("chr5", 0, 1500),
+        ("chr5", 3000, 4800),
+        ("chr5", 5000, 99000),  # Spans multiple 100kb bins
+        ("chr5", 100000, 101500),  # Starts at the beginning of a new 100kb bin
+        ("chr5", 101501, 200000),  # Spans multiple 100kb bins, starts in one and ends in another
+        ("chr5", 200001, 300000),  # Spans multiple 100kb bins, starts in one and ends in another
+        ("chr5", 300000, 400000),  # Ends right at the boundary of a 100kb bin
+        ("chr5", 400001, 500000),  # Starts right at the beginning of a new 100kb bin
+    ]
+    # fmt: on
+    bed_file = tmpdir / "test.bed"
+    with open(bed_file, "w", encoding="utf8") as file:
+        for line in bed_data:
+            file.write("\t".join(map(str, line)) + "\n")
+    return bed_file
+
+
+def test_load_bed(real_test_bed):
     """Tests the `load_bed` function."""
-    bed_data = load_bed(test_bed)
-    # Add your assertions here based on what you expect in your bed file
+    bed_data = load_bed(real_test_bed)
     assert bed_data is not None
     assert len(bed_data) > 0
 
 
-def test_preprocess_bed(test_bed):
+def test_preprocess_bed(real_test_bed):
     """Tests the `preprocess_bed` function."""
-    bed_data = load_bed(test_bed)
+    bed_data = load_bed(real_test_bed)
     processed_data = preprocess_bed(bed_data)
-    # Add your assertions here based on what you expect after processing your bed file
     assert processed_data is not None
     assert len(processed_data) > 0
 
 
-def test_process_file(tmpdir, test_bed, test_hdf5):
+def test_get_positions_to_treat(mock_test_bed):
+    """Tests the `get_positions_to_treat` function."""
+    bed_data = preprocess_bed(load_bed(mock_test_bed))
+    positions_to_treat = get_positions_to_treat(bed_data, bin_resolution=100000)
+    assert positions_to_treat is not None
+    assert len(positions_to_treat) > 0
+
+
+@pytest.mark.parametrize("test_bed", ["mock_test_bed", "real_test_bed"])
+def test_process_file(tmpdir, test_bed, test_hdf5, request):
     """Tests the `process_file` function."""
-    bed_data = load_bed(test_bed)
-    processed_data = preprocess_bed(bed_data)
+    test_bed = request.getfixturevalue(test_bed)  # get the bed file path
+
+    # Compute the positions of zeroes in the input HDF5 before processing
+    initial_zero_positions = {}
+    with h5py.File(test_hdf5, "r") as f:
+        header = list(f.keys())[0]
+        hdf5_data: h5py.Group = f[header]  # type: ignore
+        for chrom, dataset in hdf5_data.items():
+            data = dataset[:]
+            zero_positions = np.where(data == 0)[0]
+            initial_zero_positions[chrom] = set(zero_positions)
+            # print(f"{chrom}, initial zeros: {len(set(zero_positions))}")
+
+    # Perform the processing
+    positions_to_treat = get_positions_to_treat(
+        preprocess_bed(load_bed(test_bed)), bin_resolution=100000
+    )
+
+    # Compute the number of modifications that should be made (new zeroes)
+    real_modifications = sum(
+        len(set(to_treat) - set(initial_zeros))
+        for to_treat, initial_zeros in zip(
+            positions_to_treat.values(), initial_zero_positions.values()
+        )
+    )
 
     output_dir = Path(tmpdir.mkdir("sub"))
-    process_file(test_hdf5, processed_data, output_dir)
+    process_file(test_hdf5, positions_to_treat, output_dir)
 
+    # Ensure the output file was created
     output_file_path = output_dir / (test_hdf5.stem + "_0blklst.hdf5")
-    # Make assertions about the output file
     assert output_file_path.is_file()
 
+    # Check that the data in the new file is as expected
+    with h5py.File(output_file_path, "r") as f:
+        header = list(f.keys())[0]
+        hdf5_data: h5py.Group = f[header]  # type: ignore
 
-def test_main(tmpdir, test_bed, test_hdf5, mocker):
+        final_zero_positions = {}
+        for chrom, dataset in hdf5_data.items():
+            data = dataset[:]
+
+            # Get positions of new zeroes in the output HDF5
+            zero_positions = np.where(data == 0)[0]
+            final_zero_positions[chrom] = set(zero_positions)
+            # print(f"{chrom}, final zeros: {len(zero_positions)}")
+
+            # Get indices for this chromosome
+            indices_to_treat = positions_to_treat.get(chrom, [])
+
+            # Check that indices_to_treat are set to zero in the dataset
+            assert np.all(data[indices_to_treat] == 0)
+
+    new_zeros_count = sum(
+        len(new - initial)
+        for new, initial in zip(
+            final_zero_positions.values(), initial_zero_positions.values()
+        )
+    )
+    assert new_zeros_count == real_modifications
+
+
+def test_main(tmpdir, mock_test_bed, test_hdf5, mocker):
     """Tests the `main` function."""
     tmpdir = Path(tmpdir)
     hdf5_list_file = tmpdir / "hdf5_list.txt"
@@ -107,7 +158,7 @@ def test_main(tmpdir, test_bed, test_hdf5, mocker):
             """Mock arguments."""
 
             hdf5_list = hdf5_list_file
-            bed_filter = test_bed
+            bed_filter = mock_test_bed
             output_dir = tmpdir
             n_jobs = 1
 
