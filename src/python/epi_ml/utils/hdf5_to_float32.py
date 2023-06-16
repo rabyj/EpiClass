c0cdca9e207840aa5d5b00ac9f5917c3bbab7964
rabyj <Joanny.Raby@usherbrooke.ca>
2023-06-16 14:34:10 -0400

    hdf5_to_float32: use logging module, add tests

--- src/python/epi_ml/utils/hdf5_to_float32.py
+++ src/python/epi_ml/utils/hdf5_to_float32.py
@@ -4,10 +4,10 @@ This module provides functionalities to copy HDF5 files to a new directory, conv
 from __future__ import annotations
 
 import argparse
+import logging
 import shutil
 import subprocess
 import traceback
-import warnings
 from pathlib import Path
 
 import h5py
@@ -16,39 +16,34 @@ import numpy as np
 from epi_ml.argparseutils.DefaultHelpParser import DefaultHelpParser as ArgumentParser
 from epi_ml.argparseutils.directorychecker import DirectoryChecker
 
+# Setting up logging configuration
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def parse_arguments() -> argparse.Namespace:
     """argument parser for command line"""
-    # fmt: off
     arg_parser = ArgumentParser()
     arg_parser.add_argument(
         "hdf5_list", type=Path, help="A file with hdf5 filenames. Use absolute path!"
     )
-
     arg_parser.add_argument(
         "output_dir",
         type=DirectoryChecker(),
         help="Directory where to create to output the new hdf5 files.",
     )
-    # fmt: on
     return arg_parser.parse_args()
 
 
 def copy_hdf5_file(file_path: Path, logdir: Path) -> Path | None:
     """
     Copies an HDF5 file to a new location, appending "_float32.hdf5" to the filename.
-
-    Args:
-        file_path (Path): The path to the original HDF5 file.
-        logdir (Path): The directory to copy the HDF5 file to.
-
-    Returns:
-        Path or None: The path to the copied file if successful, or None if the file already exists at the new location.
     """
     new_hdf5_path = logdir / (file_path.stem + "_float32.hdf5")
 
     if new_hdf5_path.is_file():
-        warnings.warn(f"{new_hdf5_path} already exists. Skipping.")
+        logging.warning("%s already exists. Skipping.", new_hdf5_path)
         return None
 
     shutil.copy(file_path, new_hdf5_path)
@@ -59,13 +54,9 @@ def copy_hdf5_file(file_path: Path, logdir: Path) -> Path | None:
 def cast_datasets_to_float32(file_path: Path) -> bool:
     """
     Casts all the datasets in an HDF5 file to float32 data type.
-
-    Args:
-        file_path (Path): The path to the HDF5 file.
-
-    Returns:
-        bool: True if any dataset was casted, False otherwise.
     """
+    max_casting_error = 1e-5
+
     modified = False
     with h5py.File(file_path, "r+") as f:
         for _, group in f.items():
@@ -81,11 +72,13 @@ def cast_datasets_to_float32(file_path: Path) -> bool:
                     # Verify the difference between the original dataset and the casted dataset
                     diff = np.abs(casted_dataset - og_dataset)
                     max_diff = np.max(diff)
-                    if max_diff > 1e-5:
-                        raise ValueError(
-                            f"Recasting failed, max(diff) > 1e-5: {file_path}"
+                    if max_diff > max_casting_error:
+                        logging.warning(
+                            "Recasting max(diff)=%.5f > %.5f: %s",
+                            max_diff,
+                            max_casting_error,
+                            file_path,
                         )
-
                     del group[dataset_name]
 
                     group.create_dataset(
@@ -99,14 +92,14 @@ def cast_datasets_to_float32(file_path: Path) -> bool:
 def repack_hdf5_file(file_path: Path) -> None:
     """
     Repacks an HDF5 file to reduce its size. Uses the h5repack command line tool.
-
-    Args:
-        file_path (Path): The path to the HDF5 file.
     """
     tmp_path = str(file_path)
     tmp_path = tmp_path + "_repacked.hdf5"
-    subprocess.run(["h5repack", file_path, tmp_path], check=True)
-    shutil.move(tmp_path, file_path)
+    try:
+        subprocess.run(["h5repack", str(file_path), tmp_path], check=True)
+        shutil.move(tmp_path, str(file_path))
+    except FileNotFoundError:
+        logging.error("'h5repack' command not found.")
 
 
 def main():
@@ -118,11 +111,6 @@ def main():
     hdf5_list_path = cli.hdf5_list
     logdir = cli.output_dir.resolve()
 
-    if shutil.which("h5repack") is None:
-        raise RuntimeError(
-            "The required 'h5repack' tool is not available in your system's PATH. Please install it before running this program."
-        )
-
     with open(hdf5_list_path, "r", encoding="utf8") as f:
         hdf5_files = [Path(line.strip()) for line in f if line.strip()]
 
@@ -130,23 +118,20 @@ def main():
         try:
             new_filepath = copy_hdf5_file(hdf5_file, logdir)
         except Exception as e:  # pylint: disable=broad-except
-            print(f"Error: {e}. Skipping file {hdf5_file}\n{traceback.format_exc()}")
+            logging.error(
+                "Error: %s. Skipping file %s\n%s", e, hdf5_file, traceback.format_exc()
+            )
             continue
 
         if new_filepath:
-            try:
-                modified = cast_datasets_to_float32(new_filepath)
-            except ValueError as e:
-                warnings.warn(f"Error: {e}.\n Skipping file {new_filepath}")
-                new_filepath.unlink(missing_ok=True)
-                continue
+            modified = cast_datasets_to_float32(new_filepath)
             if modified:
-                print(
-                    f"Casting and verification successful. Repacking file {new_filepath}"
+                logging.info(
+                    "Casting and verification successful. Repacking file %s", new_filepath
                 )
                 repack_hdf5_file(new_filepath)
             else:
-                print("File already existing or no casting needed. Skipping.")
+                logging.info("File already existing or no casting needed. Skipping.")
                 new_filepath.unlink(missing_ok=True)
 
 
---
+++ src/python/tests/utils/hdf5_to_float32_test.py
@@ -0,0 +1,125 @@
+"""
+This module contains tests for HDF5 file operations. These tests include copying HDF5 files,
+casting datasets within these files to the float32 data type, repacking files to reduce size,
+and a workflow that performs these operations in sequence.
+"""
+from pathlib import Path
+
+import h5py
+import numpy as np
+import pytest
+
+from epi_ml.utils.hdf5_to_float32 import (
+    cast_datasets_to_float32,
+    copy_hdf5_file,
+    repack_hdf5_file,
+)
+
+current_dir = Path(__file__).parent
+
+
+@pytest.fixture(name="test_hdf5")
+def hdf5_test_file() -> Path:
+    """
+    Provides a Path object to an existing HDF5 test file.
+
+    Returns:
+        Path: A Path object pointing to the HDF5 test file.
+    """
+    return (
+        current_dir.parent
+        / "fixtures"
+        / "89a0dcb635f0e9740f587931437b69f1_100kb_all_none_value.hdf5"
+    )
+
+
+def test_copy_hdf5_file(tmp_path, test_hdf5):
+    """
+    Test for copying an HDF5 file to a new location.
+
+    Args:
+        tmp_path (Path): Temporary path provided by pytest fixture.
+        test_hdf5 (Path): Path to the test HDF5 file.
+    """
+    copied_file_path = copy_hdf5_file(test_hdf5, tmp_path)
+    assert copied_file_path is not None
+    assert copied_file_path.is_file()
+    assert copied_file_path.stem == test_hdf5.stem + "_float32"
+
+
+def test_cast_datasets_to_float32(tmp_path, test_hdf5):
+    """
+    Test for casting datasets in an HDF5 file to float32.
+
+    Args:
+        tmp_path (Path): Temporary path provided by pytest fixture.
+        test_hdf5 (Path): Path to the test HDF5 file.
+    """
+    copied_file_path = copy_hdf5_file(test_hdf5, tmp_path)
+    assert copied_file_path is not None
+    assert cast_datasets_to_float32(copied_file_path) is not None
+
+
+def test_repack_hdf5_file(tmp_path, test_hdf5):
+    """
+    Test for repacking an HDF5 file to reduce its size.
+
+    Args:
+        tmp_path (Path): Temporary path provided by pytest fixture.
+        test_hdf5 (Path): Path to the test HDF5 file.
+    """
+    copied_file_path = copy_hdf5_file(test_hdf5, tmp_path)
+    assert copied_file_path is not None
+    cast_datasets_to_float32(copied_file_path)
+    repack_hdf5_file(copied_file_path)
+    assert copied_file_path.is_file()
+
+
+def test_workflow(tmp_path, test_hdf5):
+    """
+    Test for performing the workflow of copying, casting, and repacking an HDF5 file.
+
+    Args:
+        tmp_path (Path): Temporary path provided by pytest fixture.
+        test_hdf5 (Path): Path to the test HDF5 file.
+    """
+    copied_file_path = copy_hdf5_file(test_hdf5, tmp_path)
+    assert copied_file_path is not None
+    assert copied_file_path.is_file()
+    cast_datasets_to_float32(copied_file_path)
+    repack_hdf5_file(copied_file_path)
+    assert copied_file_path.is_file()
+
+
+def test_casting_changes_data(tmp_path):
+    """
+    Test for verifying that casting datasets in a fake HDF5 file changes the data.
+
+    Args:
+        tmp_path (Path): Temporary path provided by pytest fixture.
+    """
+    # Create a fake float64 dataset
+    original_data = np.random.uniform(low=0, high=400, size=(10, 10)).astype(np.float64)
+
+    # Create an HDF5 file with this dataset
+    original_file_path = tmp_path / "original.hdf5"
+    with h5py.File(original_file_path, "w") as f:
+        group = f.create_group("fake_data")
+        group.create_dataset("fake_dataset", data=original_data, dtype=np.float64)
+
+    # Copy the HDF5 file to a new location
+    copied_file_path = copy_hdf5_file(original_file_path, tmp_path)
+    assert copied_file_path is not None
+
+    # Perform the casting
+    cast_datasets_to_float32(copied_file_path)
+
+    # Load the dataset after casting
+    with h5py.File(copied_file_path, "r") as f:
+        casted_data = np.array(f["fake_data"]["fake_dataset"])  # type: ignore
+
+    # Check that the data type has changed to float32
+    assert casted_data.dtype == np.float32
+
+    # Check that the data has changed after casting
+    assert not np.allclose(original_data, casted_data, atol=1e-8, rtol=0)
