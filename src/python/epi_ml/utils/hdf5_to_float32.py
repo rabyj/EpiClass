rabyj/EpiClass

commit f0955fadd713855fa26c51a20e3039bc3e464668

hdf5_to_float32: check h5repack first + log all

src/python/epi_ml/utils/hdf5_to_float32.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 import shutil
 import subprocess
+import sys
 import traceback
 from pathlib import Path
 
@@ -19,7 +20,7 @@ from epi_ml.core.hdf5_loader import Hdf5Loader
 
 # Setting up logging configuration
 logging.basicConfig(
-    level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
@@ -111,6 +112,12 @@ def main():
 
     hdf5_list_path = cli.hdf5_list
     logdir = cli.output_dir.resolve()
+
+    try:
+        subprocess.run(["h5repack"], stdout=subprocess.DEVNULL, check=True)
+    except FileNotFoundError:
+        logging.error("'h5repack' command not found.")
+        sys.exit(1)
 
     hdf5_files = list(Hdf5Loader.read_list(hdf5_list_path, adapt=True).values())
 

commit 0eb711b73f5ebaa03c4e576b7a51b4fa50ec5975

Fix h5repack pre-check bug
Fixes bug introduced in f0955fadd713855fa26c51a20e3039bc3e464668

src/python/epi_ml/utils/hdf5_to_float32.py
@@ -7,7 +7,6 @@ import argparse
 import logging
 import shutil
 import subprocess
-import sys
 import traceback
 from pathlib import Path
 
@@ -113,11 +112,8 @@ def main():
     hdf5_list_path = cli.hdf5_list
     logdir = cli.output_dir.resolve()
 
-    try:
-        subprocess.run(["h5repack"], stdout=subprocess.DEVNULL, check=True)
-    except FileNotFoundError:
-        logging.error("'h5repack' command not found.")
-        sys.exit(1)
+    if shutil.which("h5repack") is None:
+        raise FileNotFoundError("'h5repack' command not found.")
 
     hdf5_files = list(Hdf5Loader.read_list(hdf5_list_path, adapt=True).values())
 