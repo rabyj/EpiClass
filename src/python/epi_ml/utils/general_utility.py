e75ae5e8952e841e476384743a7bb30bd4176e4a
rabyj <Joanny.Raby@usherbrooke.ca>
2023-07-12 17:21:30 -0400

    general utility: hdf5 file list writing

---
+++ src/python/epi_ml/utils/general_utility.py
@@ -0,0 +1,20 @@
+"""For lost little functions that don't fit anywhere else."""
+from pathlib import Path
+from typing import List
+
+
+def write_hdf5_paths_to_file(
+    md5s: List[str], prefix: str, suffix: str, path: Path
+) -> None:
+    """Write a list of md5s to a file, with a prefix and suffix."""
+    with open(path, "w", encoding="utf8") as f:
+        for md5 in md5s:
+            f.write(f"{prefix}{md5}{suffix}\n")
+
+
+def write_md5s_to_file(md5s: List[str], logdir: str, name: str) -> None:
+    """Write a list of md5s to a file."""
+    filename = Path(logdir) / f"{name}.md5"
+    with open(filename, "w", encoding="utf8") as f:
+        for md5 in md5s:
+            f.write(f"{md5}\n")
