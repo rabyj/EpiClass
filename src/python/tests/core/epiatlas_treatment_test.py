rabyj/EpiClass

commit b0d49e4577db5f2aa27a27ae56db08f2647630a8

Improve and refactor logging

src/python/epi_ml/utils/my_logging.py
@@ -8,6 +8,10 @@ from typing import Dict
 
 from pytorch_lightning import loggers as pl_loggers
 
+from epi_ml.core.data import DataSet
+from epi_ml.utils.general_utility import write_md5s_to_file
+from epi_ml.utils.time import time_now_str
+
 
 def log_pre_training(
     logger: pl_loggers.CometLogger, to_log: Dict[str, str], step: int | None
@@ -55,3 +59,60 @@ def log_pre_training(
     commit_label = subprocess.check_output(["git", "describe"], encoding="UTF-8").strip()
     print(f"The current commit is {commit_label}")
     logger.experiment.log_other("Code version / commit", f"{commit_label}")
+
+
+def log_dset_composition(
+    my_data: DataSet,
+    logdir: str | Path | None,
+    logger: pl_loggers.CometLogger | None,
+    split_nb: int,
+):
+    """Logs dataset composition to logger and file.
+
+    Either the 'logdir' or 'logger' parameter must be provided, otherwise ValueError is raised.
+
+    This function logs
+    - training/validation set size
+    - total number of unique files (training + validation),
+    - training/valid unique md5 composition (to a file)
+
+    Args:
+        my_data (DataSet): The dataset to log.
+        logdir (str | Path | None): The directory where logs will be stored. None if logger is given.
+        logger (pl_loggers.CometLogger | None): The logger object to use for logging. None if logdir is given.
+        split_nb (int): The split number to log.
+
+    Raises:
+        ValueError if:
+        - both 'logdir' and 'logger' parameters are None
+        - train and validation sets overlap.
+    """
+    if logger:
+        save_dir = logger.save_dir
+    elif logdir:
+        save_dir = logdir
+    else:
+        raise ValueError("No logdir or logger given.")
+
+    for dset_name in ["train", "validation", "test"]:
+        size = getattr(my_data, dset_name).num_examples
+        if logger:
+            logger.experiment.log_other(f"{dset_name} size", size)
+        print(f"Split {split_nb} {dset_name} size: {size}")
+
+    ids_train = set(my_data.train.ids.tolist())
+    ids_valid = set(my_data.validation.ids.tolist())
+    if not ids_train & ids_valid:
+        raise ValueError("Train and validation sets overlap")
+
+    nb_files = len(ids_train) + len(ids_valid)
+    if logger:
+        logger.experiment.log_other("Total nb of files", nb_files)
+    if split_nb == 0:
+        print(f"Total nb of files: {nb_files}")
+
+    # Save exactly which files are used for training and validation
+    for ids, name in zip([ids_train, ids_valid], ["training", "validation"]):
+        if len(ids) > 0:
+            name = f"split{split_nb}_{name}_{time_now_str()}"
+            write_md5s_to_file(sorted(ids), save_dir, name)  # type: ignore

src/python/epi_ml/core/estimators.py
@@ -32,6 +32,7 @@ from epi_ml.core.analysis import write_pred_table
 from epi_ml.core.data import DataSet
 from epi_ml.core.epiatlas_treatment import EpiAtlasFoldFactory
 from epi_ml.utils.check_dir import create_dirs
+from epi_ml.utils.my_logging import log_dset_composition
 from epi_ml.utils.time import time_now
 
 NFOLD_TUNE = 9
@@ -421,12 +422,7 @@ def run_prediction(
       logdir (Path): The directory where the results will be saved.
       verbose: Whether to print out the metrics. Defaults to True
     """
-    if verbose:
-        print(f"Split {i} training size: {my_data.train.num_examples}")
-
-    if i == 0:
-        nb_files = len(set(my_data.train.ids.tolist() + my_data.validation.ids.tolist()))
-        print(f"Total nb of files: {nb_files}")
+    log_dset_composition(my_data, logdir=logdir, logger=None, split_nb=i)
 
     estimator.fit(X=my_data.train.signals, y=my_data.train.encoded_labels)
 

src/python/epi_ml/epiatlas_training.py
@@ -28,7 +28,7 @@ from epi_ml.core.model_pytorch import LightningDenseClassifier
 from epi_ml.core.trainer import MyTrainer, define_callbacks
 from epi_ml.utils import modify_metadata
 from epi_ml.utils.check_dir import create_dirs
-from epi_ml.utils.my_logging import log_pre_training
+from epi_ml.utils.my_logging import log_dset_composition, log_pre_training
 from epi_ml.utils.time import time_now
 
 
@@ -215,11 +215,7 @@ def do_one_experiment(
     """Wrapper for convenience. Skip training if restore is True"""
     begin_loop = time_now()
 
-    logger.experiment.log_other("Training size", my_data.train.num_examples)
-    print(f"Split {split_nb} training size: {my_data.train.num_examples}")
-
-    nb_files = len(set(my_data.train.ids.tolist() + my_data.validation.ids.tolist()))
-    logger.experiment.log_other("Total nb of files", nb_files)
+    log_dset_composition(my_data, logdir=None, logger=logger, split_nb=split_nb)
 
     dsets_dict = create_torch_datasets(
         data=my_data,

src/python/epi_ml/epiatlas_training_no_valid.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 import argparse
-import gc
 import json
 import os
 import sys
@@ -29,7 +28,7 @@ from epi_ml.core.model_pytorch import LightningDenseClassifier
 from epi_ml.core.trainer import MyTrainer, define_callbacks
 from epi_ml.utils.check_dir import create_dirs
 from epi_ml.utils.modify_metadata import filter_by_pairs, fix_roadmap, merge_pair_end_info
-from epi_ml.utils.my_logging import log_pre_training
+from epi_ml.utils.my_logging import log_dset_composition, log_pre_training
 from epi_ml.utils.time import time_now
 
 
@@ -181,11 +180,8 @@ def train_without_valid(
     restore: bool,
 ) -> None:
     """Wrapper for convenience. Skip training if restore is True"""
-    logger.experiment.log_other("Training size", my_data.train.num_examples)
-    print(f"training size: {my_data.train.num_examples}")
 
-    nb_files = len(set(my_data.train.ids.tolist()))
-    logger.experiment.log_other("Total nb of files", nb_files)
+    log_dset_composition(my_data, logdir=None, logger=logger, split_nb=0)
 
     train_dataset, train_dataloader = create_torch_datasets(
         data=my_data,
@@ -269,10 +265,7 @@ def train_without_valid(
         print(f"training time: {training_time}")
 
         # reload comet logger for further logging, will create new experience in offline mode
-        if type(logger.experiment).__name__ == "OfflineExperiment":
-            IsOffline = True
-        else:
-            IsOffline = False
+        IsOffline = bool(type(logger.experiment).__name__ == "OfflineExperiment")
 
         logger = pl_loggers.CometLogger(
             project_name="EpiLaP",

commit 6d4eb4cfd68c0bf242d7163995221f886e141082

Moar tests: fix logging error from last commit
Fix logging error that will kill all mains introduced in b0d49e4577db5f

may a higher being have mercy on me, I created convoluted tests
to check a new error with new logging code, instead
of trusting my previous tests and just looking more deeply at the
logging error. It was a one line boolean logic error.

SIGH

src/python/epi_ml/core/epiatlas_treatment.py
@@ -199,6 +199,8 @@ class EpiAtlasFoldFactory:
         self._classes = self._epiatlas_dataset.classes
 
         self._train_val, self._test = self._reserve_test()
+        if len(self._train_val) == 0:
+            raise ValueError("No data in training and validation.")
 
     @classmethod
     def from_datasource(
@@ -287,7 +289,7 @@ class EpiAtlasFoldFactory:
     def _split_dataset(
         self, dset: data.KnownData, n_splits: int, oversample: bool = False
     ) -> Generator[Tuple[data.KnownData, data.KnownData], None, None]:
-        # Convert the labels and groups (uuids) into numpy  rays
+        # Convert the labels and groups (uuids) into numpy arrays
         uuids, uuids_unique, uuids_inverse = self._label_uuid(dset)
         labels_unique = [
             dset.encoded_labels[uuids == uuid][0] for uuid in uuids_unique

src/python/epi_ml/utils/general_utility.py
@@ -12,9 +12,10 @@ def write_hdf5_paths_to_file(
             f.write(f"{prefix}{md5}{suffix}\n")
 
 
-def write_md5s_to_file(md5s: List[str], logdir: str, name: str) -> None:
-    """Write a list of md5s to a file."""
+def write_md5s_to_file(md5s: List[str], logdir: str, name: str) -> Path:
+    """Write a list of md5s to a file. Return filepath."""
     filename = Path(logdir) / f"{name}.md5"
     with open(filename, "w", encoding="utf8") as f:
         for md5 in md5s:
             f.write(f"{md5}\n")
+    return filename

src/python/epi_ml/utils/my_logging.py
@@ -102,7 +102,7 @@ def log_dset_composition(
 
     ids_train = set(my_data.train.ids.tolist())
     ids_valid = set(my_data.validation.ids.tolist())
-    if not ids_train & ids_valid:
+    if ids_train & ids_valid:
         raise ValueError("Train and validation sets overlap")
 
     nb_files = len(ids_train) + len(ids_valid)

src/python/tests/core/epiatlas_treatment_test.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import copy
 from collections import Counter
+from pathlib import Path
 from typing import List
 
 import numpy as np
@@ -17,6 +18,7 @@ from epi_ml.core.epiatlas_treatment import (
     EpiAtlasFoldFactory,
 )
 from epi_ml.core.metadata import Metadata
+from epi_ml.utils.general_utility import write_md5s_to_file
 from tests.fixtures.epilap_test_data import EpiAtlasTreatmentTestData
 
 
@@ -26,10 +28,35 @@ class TestEpiAtlasFoldFactory:
     Preconditions: Exact same input labels list. (raw_dset.train.encoded_labels)
     """
 
-    @pytest.fixture(scope="class", autouse=True)
+    @pytest.fixture(scope="class", name="test_data")
     def test_data(self) -> EpiAtlasFoldFactory:
         """Mock test EpiAtlasFoldFactory."""
         return EpiAtlasTreatmentTestData.default_test_data()
+
+    @pytest.fixture(name="big_test_data")
+    def big_test_data(self, tmp_path) -> EpiAtlasFoldFactory:
+        """Mock big EpiAtlasFoldFactory."""
+        tmp_path = Path(tmp_path) / "big_test"
+        tmp_path.mkdir(exist_ok=True, parents=True)
+
+        target_category = "harmonized_donor_sex"
+
+        meta_path = (
+            Path.home()
+            / "projects/epilap/input/metadata/hg38_2023_epiatlas_dfreeze_limited_categories.json"
+        )
+        # Reduce total nb of files
+        meta = Metadata(meta_path)
+        meta.select_category_subsets(target_category, ["female", "male"])
+        md5_per_class = meta.md5_per_class(target_category)
+        md5s = [md5 for md5_list in md5_per_class.values() for md5 in md5_list[:2000]]
+
+        md5_file = write_md5s_to_file(md5s=md5s, logdir=str(tmp_path), name="big_test")
+        return EpiAtlasTreatmentTestData(
+            metadata_path=meta_path,
+            logdir=tmp_path,
+            md5_list_path=md5_file,
+        ).get_ea_handler(label_category=target_category)
 
     @pytest.fixture(scope="class")
     def test_metadata(self, test_data) -> Metadata:
@@ -43,6 +70,22 @@ class TestEpiAtlasFoldFactory:
         return test_data.epiatlas_dataset.datasource
 
     @staticmethod
+    def test_data_from_datasource(
+        datasource: EpiDataSource, metadata: Metadata, target_category: str
+    ) -> EpiAtlasFoldFactory:
+        """Create EpiAtlasFoldFactory from datasource."""
+        ea_handler = EpiAtlasFoldFactory.from_datasource(
+            datasource,
+            label_category=target_category,
+            min_class_size=1,
+            n_fold=2,
+            md5_list=list(metadata.md5s),
+            force_filter=True,
+            test_ratio=0,
+        )
+        return ea_handler
+
+    @staticmethod
     def modified_metadata(test_metadata, del_tracks: List[str]) -> Metadata:
         """Remove tracks from metadata."""
         meta = copy.deepcopy(test_metadata)
@@ -52,15 +95,44 @@ class TestEpiAtlasFoldFactory:
                     del meta[md5]
         return meta
 
-    def test_yield_split_size(self, test_data: EpiAtlasFoldFactory):
-        """Test that splits contain the correct number of training and validation samples."""
-        total_data = test_data.train_val_dset
+    @staticmethod
+    def assert_splits(ea_handler: EpiAtlasFoldFactory):
+        """Assert that splits are correct."""
+        total_data = ea_handler.train_val_dset
         assert total_data.num_examples == len(set(total_data.ids))
 
-        for dset in test_data.yield_split():
-            train_unique_size = len(set(dset.train.ids))
-            valid_unique_size = len(set(dset.validation.ids))
-            assert total_data.num_examples == train_unique_size + valid_unique_size
+        for dset in ea_handler.yield_split():
+            trains_ids = set(dset.train.ids)
+            valid_ids = set(dset.validation.ids)
+            assert len(trains_ids & valid_ids) == 0, "No overlap between train and valid"
+
+            assert len(valid_ids) == len(dset.validation.ids), "No dups in validation"
+
+            assert dset.test.num_examples == 0, "No test set"
+
+            assert total_data.num_examples == len(trains_ids) + len(valid_ids)
+
+    # @pytest.mark.parametrize("splitter", ["test_data", "big_test_data"])
+    def test_yield_correct_split_1(self, test_data: EpiAtlasFoldFactory):
+        """Test that splits contain the correct number of training and validation samples."""
+        ea_handler = test_data
+
+        datasource = test_data.epiatlas_dataset.datasource
+        metadata = test_data.epiatlas_dataset.metadata
+        ea_handler2 = TestEpiAtlasFoldFactory.test_data_from_datasource(
+            datasource,
+            metadata,
+            target_category=test_data.epiatlas_dataset.target_category,
+        )
+
+        for handler in [ea_handler, ea_handler2]:
+            TestEpiAtlasFoldFactory.assert_splits(handler)
+
+    @pytest.mark.filterwarnings("ignore:.*Cannot read file directly.*")
+    @pytest.mark.skip(reason="Takes too long.")
+    def test_yield_correct_split_2(self, big_test_data: EpiAtlasFoldFactory):
+        """Test that splits contain the correct number of training and validation samples, with real metadata."""
+        TestEpiAtlasFoldFactory.assert_splits(big_test_data)
 
     # def test_yield_subsample_validation_1(self, test_data: EpiAtlasFoldFactory):
     #     """Test correct subsampling. Subsplit should partition initial validation split."""
@@ -161,9 +233,11 @@ class TestEpiAtlasFoldFactory:
 
         valid_raw_sum = 0
         for dset in ea_handler.yield_split():
-            train_unique_size = len(set(dset.train.ids))
-            valid_unique_size = len(set(dset.validation.ids))
-            assert dset.test.num_examples == 0
+            trains_ids = set(dset.train.ids)
+            valid_ids = set(dset.validation.ids)
+
+            train_unique_size = len(trains_ids)
+            valid_unique_size = len(valid_ids)
 
             assert total_size == train_unique_size + valid_unique_size
 

src/python/tests/fixtures/epilap_test_data.py
@@ -1,6 +1,8 @@
 """EpiAtlas data treatment testing module."""
 from __future__ import annotations
 
+import multiprocessing
+import os
 from pathlib import Path
 from typing import List
 
@@ -43,17 +45,43 @@ class EpiAtlasTreatmentTestData:
             n_fold=n_fold,
         )
 
+    @staticmethod
+    def _create_symlink(source: Path, link_name: Path):
+        """Create a symbolic link pointing to source named link_name"""
+        try:
+            os.symlink(source, link_name)
+        except FileExistsError:
+            pass
+
     def create_temp_hdf5s(
         self, md5_list_path: Path, name="_100kb_all_none_value.hdf5"
     ) -> List[Path]:
         """Create temporary files and returns paths"""
         tmp_files = []
         with open(md5_list_path, "r", encoding="utf8") as md5_list:
-            for md5 in md5_list.readlines():
+            md5s = [md5.strip() for md5 in md5_list.readlines()]
+
+        if len(md5s) < 100:
+            for md5 in md5s:
                 md5 = md5.strip()
                 tmp_file = self.hdf5_logdir / f"{md5 + name}"
                 tmp_files.append(tmp_file)
                 self.write_mock_hdf5(tmp_file, md5)
+        else:
+            md5 = md5s[0]
+            real_tmp_file = self.hdf5_logdir / f"{md5 + name}"
+            tmp_files.append(real_tmp_file)
+            self.write_mock_hdf5(real_tmp_file, md5)
+
+            for md5 in md5s[1:]:
+                tmp_file = self.hdf5_logdir / f"{md5 + name}"
+                tmp_files.append(tmp_file)
+
+            with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+                pool.starmap(
+                    self._create_symlink,
+                    [(real_tmp_file, fake_file) for fake_file in tmp_files[1:]],
+                )
 
         return tmp_files
 
@@ -108,7 +136,7 @@ def create_test_metadata(metadata_source_path: Path, md5_list: Path):
     my_metadata = Metadata(metadata_source_path)
 
     with open(md5_list, "r", encoding="utf8") as f:
-        md5_set = set([md5.strip() for md5 in f.readlines()])
+        md5_set = set(md5.strip() for md5 in f.readlines())
 
     for md5 in list(my_metadata.md5s):
         if md5 not in md5_set: