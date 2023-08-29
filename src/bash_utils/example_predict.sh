e5a1b03b20aea0f027b155d7112d61bce05c3671
rabyj <Joanny.Raby@usherbrooke.ca>
2023-08-29 16:34:00 -0400

    Update example file

--- src/bash_utils/example_predict.sh
+++ src/bash_utils/example_predict.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
 #SBATCH --account=your-account
-#SBATCH --job-name=predict-sex
+#SBATCH --job-name=name-of-your-job
 #SBATCH --output=./slurm_files/%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --mem=400G
 #SBATCH --mail-user=john.doe@domain.ca
 #SBATCH --mail-type=END,FAIL
+
+
+# AT LEAST MODIFY THE PATHS IN BETWEEN "--", e.g. gen_path and path-to-venv
 
 export PYTHONUNBUFFERED=TRUE
 
@@ -19,12 +22,12 @@ then
   echo "print =========================================="
 fi
 
-gen_path="/home/rabyj/project-rabyj"
+gen_path="/home/rabyj/--project-path--"
 input_path="${gen_path}/epilap/input"
 output_path="${gen_path}/epilap/output/logs"
 
 # use correct environment
-source ${gen_path}/epilap/venv-torch/bin/activate
+source "--path-to-venv--/bin/activate"
 
 # --- choose category + hparams + source files ---
 # MODIFY THINGS HERE
@@ -55,7 +58,7 @@ chromsizes="${input_path}/chromsizes/hg38.noy.chrom.sizes"
 out1="${log}/output_job${SLURM_JOB_ID}_${SLURM_JOB_NAME}_${timestamp}.o"
 out2="${log}/output_job${SLURM_JOB_ID}_${SLURM_JOB_NAME}_${timestamp}.e"
 
-program_path="${gen_path}/sources/epi_ml/epi_ml"
+program_path="${gen_path}/sources/epi_ml/src/python/epi_ml"
 cd ${program_path}
 
 
@@ -63,34 +66,25 @@ cd ${program_path}
 set -e
 
 printf '\n%s\n' "Launching following command"
-printf '%s\n' "python ${program_path}/python/utils/check_dir.py ${log}"
-python ${program_path}/python/utils/check_dir.py ${log}
+printf '%s\n' "python ${program_path}/utils/check_dir.py ${log}"
+python ${program_path}/utils/check_dir.py ${log}
 
 printf '\n%s\n' "Launching following command"
-printf '%s\n' "python ${program_path}/python/utils/check_dir.py --exists ${model}"
-python ${program_path}/python/utils/check_dir.py --exists ${model}
-
-
-if [ X"$SLURM_STEP_ID" = "X" -a X"$SLURM_PROCID" = "X"0 ]
-then
-  newdir="$SLURM_TMPDIR/hdf5s/"
-  mkdir $newdir
-  filelist=$(cat ${arg3})
-    for FILE in ${filelist}
-    do
-      cp ${FILE} ${newdir}
-    done
-fi
+printf '%s\n' "python ${program_path}/utils/check_dir.py --exists ${model}"
+python ${program_path}/utils/check_dir.py --exists ${model}
 
 
 # --- launch ---
-# printf '\n%s\n' "Launching following command"
-# printf '%s\n' "python ${program_path}/python/predict.py ${hdf5_list} ${chromsizes} ${log}  --model ${model} > ${out1} 2> ${out2}"
-# python ${program_path}/python/predict.py ${hdf5_list} ${chromsizes} ${log}  --model ${model} > ${out1} 2> ${out2}
-
-to_augment="${log}/test_prediction.csv"
-metadata="${input_path}/metadata/${dataset}_harmonizedv8.json"
-
 printf '\n%s\n' "Launching following command"
-printf '%s\n' "python ${program_path}/python/utils/augment_predict_file.py ${to_augment} ${metadata} --all-categories"
-python ${program_path}/python/utils/augment_predict_file.py ${to_augment} ${metadata} --all-categories
+printf '%s\n' "python ${program_path}/predict.py ${hdf5_list} ${chromsizes} ${log} --model ${model} > ${out1} 2> ${out2}"
+python ${program_path}/predict.py ${hdf5_list} ${chromsizes} ${log} --model ${model} > ${out1} 2> ${out2}
+
+
+# -- You could then augment the prediction file with new metadata if it is known --
+
+# to_augment="${log}/test_prediction.csv"
+# metadata="${input_path}/metadata/${dataset}_harmonizedv8.json"
+
+# printf '\n%s\n' "Launching following command"
+# printf '%s\n' "python ${program_path}/utils/augment_predict_file.py ${to_augment} ${metadata} --all-categories"
+# python ${program_path}/utils/augment_predict_file.py ${to_augment} ${metadata} --all-categories
