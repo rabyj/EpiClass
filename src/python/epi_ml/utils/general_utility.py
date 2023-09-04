50397bc94b6103d0508d552df1dacc473dae84f2
rabyj <Joanny.Raby@usherbrooke.ca>
2023-09-04 16:01:49 -0400

    Change write_hdf5_paths_to_file

--- src/python/epi_ml/utils/general_utility.py
+++ src/python/epi_ml/utils/general_utility.py
@@ -1,21 +1,23 @@
 """For lost little functions that don't fit anywhere else."""
+from __future__ import annotations
+
 from pathlib import Path
 from typing import Iterable
 
 
 def write_hdf5_paths_to_file(
-    md5s: Iterable[str], parent: str, suffix: str, filepath: Path
+    md5s: Iterable[str], parent: str, suffix: str, filepath: str | Path
 ) -> None:
     """Write a list of md5s to a file, with a prefix and suffix.
-    Path(prefix)/md5{suffix}.hdf5
+    Path(prefix)/{md5}_{suffix}_value.hdf5
     """
     with open(filepath, "w", encoding="utf8") as f:
         for md5 in md5s:
-            line = Path(parent) / (md5 + f"{suffix}.hdf5\n")
+            line = Path(parent) / (f"{md5}_{suffix}_value.hdf5\n")
             f.write(str(line))
 
 
-def write_md5s_to_file(md5s: Iterable[str], logdir: str, name: str) -> Path:
+def write_md5s_to_file(md5s: Iterable[str], logdir: str | Path, name: str) -> Path:
     """Write a list of md5s to a file. Return filepath."""
     filename = Path(logdir) / f"{name}.md5"
     with open(filename, "w", encoding="utf8") as f:
