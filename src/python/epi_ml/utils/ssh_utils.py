5b9563f5815cadbd85a26b122eca10a873a50d55
rabyj <Joanny.Raby@usherbrooke.ca>
2023-10-04 15:37:14 -0400

    update merge dfreeze results

---
+++ src/python/epi_ml/utils/ssh_utils.py
@@ -0,0 +1,74 @@
+"""Utility functions for SSH and SCP."""
+from __future__ import annotations
+
+from pathlib import Path
+from typing import List
+
+import paramiko
+from scp import SCPClient
+
+
+def createSSHClient(hostname: str, port: int, username: str):
+    """Create SSH client from paramiko. Needs to be closed later."""
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    ssh_key_filepath = f"{str(Path.home())}/.ssh/id_ed25519"
+    client.connect(
+        hostname=hostname, port=port, username=username, key_filename=ssh_key_filepath
+    )
+    return client
+
+
+def createSCPClient(ssh_client: paramiko.SSHClient):
+    """Create SCP client from paramiko. Needs to be closed later.
+
+    Use this with scp.get and scp.put.
+    """
+    return SCPClient(ssh_client.get_transport())  # type: ignore
+
+
+def run_commands_via_ssh(
+    cmds: List[str], hostname: str, port: int, username: str
+) -> List:
+    """
+    Run a command on a remote server via SSH using SSH key authentication and return the decoded result.
+
+    Args:
+        hostname (str): The hostname of the remote server.
+        port (int): The port number for the SSH service.
+        username (str): The username for SSH login.
+    Returns:
+        ;ist: The decoded output of the commands.
+    """
+    client = createSSHClient(hostname, port, username)
+    results = []
+    for cmd in cmds:
+        _, stdout, _ = client.exec_command(cmd)
+        result = stdout.read().decode("utf-8")
+        results.append(result)
+
+    client.close()
+
+    return results
+
+
+# unknown_files = (
+#     subprocess.check_output(
+#         (
+#             "find",
+#             f"{gen_base_dir}",
+#             "-mindepth",
+#             "5",
+#             "-maxdepth",
+#             "6",
+#             "-type",
+#             "d",
+#             "-name",
+#             "predictions",
+#         )
+#     )
+#     .decode("utf-8")
+#     .splitlines()
+# )
