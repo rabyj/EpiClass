1d372ed59509ca0f2254012b89a6d72137601527
rabyj <Joanny.Raby@usherbrooke.ca>
2024-03-12 17:05:57 -0400

    New comet-ml results collection method

--- src/python/epi_ml/utils/time.py
+++ src/python/epi_ml/utils/time.py
@@ -1,3 +1,5 @@
+"""Time utilities"""
+
 from datetime import datetime
 
 
@@ -7,4 +9,10 @@ def time_now() -> datetime:
 
 
 def time_now_str() -> str:
+    """Return datetime of call as a string in the format YYYY-MM-DD_HH-MM-SS"""
     return time_now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def seconds_to_str(seconds: int) -> str:
+    """Convert seconds to a string in the format HH:MM:SS"""
+    return str(datetime.utcfromtimestamp(seconds).strftime("%H:%M:%S"))
