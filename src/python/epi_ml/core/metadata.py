rabyj/EpiClass

commit c552b6a7511b20c043483459ad1097fdbbc8d639

New ValueError for metadata nan float bug.
Prevents further problems with identifying a nan float in the metadata.
This kind of error was introduced when trying to create chip-seq_imputed metadata.

src/python/epi_ml/core/metadata.py
@@ -1,4 +1,5 @@
 """Module from Metadata class and HealthyCategory."""
+
 # pylint: disable=unnecessary-lambda-assignment,too-many-public-methods
 from __future__ import annotations
 
@@ -6,6 +7,7 @@ import copy
 import json
 import marshal
 import os
+import sys
 from collections import Counter, defaultdict
 from collections.abc import ItemsView, KeysView, ValuesView
 from difflib import SequenceMatcher as SM
@@ -224,6 +226,16 @@ class Metadata:
         sorted_md5 = sorted(self.md5s)
         uniq = set()
         for md5 in sorted_md5:
+            val = self[md5].get(label_category)
+
+            # Everything should be a string, this was added because there was a bug with a nan object treated as a float
+            if not isinstance(val, str):
+                print(
+                    f"md5: {md5} has non-string label of type {type(val)}: {val}",
+                    file=sys.stderr,
+                )
+                raise ValueError("Non-string label for {label_category} at {md5}.")
+
             uniq.add(self[md5].get(label_category))
         uniq.discard(None)
         return sorted(list(uniq))

commit 2faddfe56ce30979476397e945f80d381aa59e37

Avoid metadata missing category label problem.
Root cause still not solved. Bug introduced in c552b6a.

Happens for example when encode metadata doesn't have the expected
category label even though that metadata is not necessarely used.
Bug was introduced when trying to fix a 'float' nan problem.

src/python/epi_ml/core/metadata.py
@@ -234,7 +234,7 @@ class Metadata:
                     f"md5: {md5} has non-string label of type {type(val)}: {val}",
                     file=sys.stderr,
                 )
-                raise ValueError("Non-string label for {label_category} at {md5}.")
+                raise ValueError(f"Non-string label for {label_category} at {md5}.")
 
             uniq.add(self[md5].get(label_category))
         uniq.discard(None)

src/python/epi_ml/epiatlas_training.py
@@ -103,6 +103,7 @@ def main():
     my_metadata.remove_category_subsets(
         label_category="track_type", labels=["Unique.raw"]
     )
+    my_metadata.remove_missing_labels(category)
 
     if category in {"paired", "paired_end_mode"}:
         category = "paired_end_mode"