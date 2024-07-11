rabyj/EpiClass

commit 7b3e52b59a066f9417ba6d2760e4e84304631256

More flexible shap run prep script (cli arg added)

src/bash_utils/compute_shaps_template.sh
@@ -46,7 +46,6 @@ fi
 model="NN" # IMPORTANT
 
 category=:::category::: # IMPORTANT
-resolution="100kb" # IMPORTANT
 
 # -- NN --
 
@@ -102,16 +101,17 @@ fi
 
 if [[ -n "$SLURM_JOB_ID" ]];
 then
-  hdf5s_location="/lustre06/project/6007515/ihec_share/local_ihec_data/epiatlas/hg38/hdf5"
-  name="epiatlas_dfreeze_${resolution}_all_none"
-  tar_file="${hdf5s_location}/${name}.tar"  # IMPORTANT
+  tar_file=":::full_tar_path:::"  # IMPORTANT
 
   cd $SLURM_TMPDIR
 
   echo "Untaring $tar_file in $SLURM_TMPDIR"
   tar -xf $tar_file
 
-  export HDF5_PARENT="${name}" # IMPORTANT
+  folder_name=$(find . -mindepth 1 -type d | head -n 1)
+  folder_name=$(basename $folder_name)
+  echo "Using folder_name: $folder_name"
+  export HDF5_PARENT="${folder_name}" # IMPORTANT
 fi
 
 

src/python/epi_ml/utils/general_utility.py
@@ -3,19 +3,24 @@ from __future__ import annotations
 
 import re
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List
 
 
 def write_hdf5_paths_to_file(
     md5s: Iterable[str], parent: str, suffix: str, filepath: str | Path
-) -> None:
+) -> List[Path]:
     """Write a list of md5s to a file, with a prefix and suffix.
-    Path(prefix=parent)/{md5}_{suffix}_value.hdf5
+    Creates files of the format: Path(prefix=parent)/{md5}_{suffix}.hdf5
+
+    Returns a list of the paths written to the file.
     """
+    files = []
     with open(filepath, "w", encoding="utf8") as f:
         for md5 in md5s:
-            line = Path(parent) / (f"{md5}_{suffix}_value.hdf5\n")
-            f.write(str(line))
+            path = Path(parent) / (f"{md5}_{suffix}.hdf5")
+            f.write(f"{path}\n")
+            files.append(path)
+    return files
 
 
 def write_md5s_to_file(md5s: Iterable[str], logdir: str | Path, name: str) -> Path:

src/python/epi_ml/utils/shap/prep_shap_run.py
@@ -9,6 +9,7 @@ import os
 import random
 import shutil
 import sys
+import tarfile
 from collections import defaultdict
 from pathlib import Path
 from typing import Iterable, List, Tuple
@@ -158,6 +159,10 @@ def parse_arguments() -> argparse.Namespace:
         "base_logdir", type=DirectoryChecker(), help="Directory where different fold directories are present"
     )
     parser.add_argument(
+        "full_tar_path", type=Path, help="tar path to full dataset. Archive should have a parent folder."
+    )
+
+    parser.add_argument(
         "--overwrite", action="store_true", help="Overwrite existing files."
         )
     parser.add_argument(
@@ -179,6 +184,7 @@ def main():
     base_logdir = cli.base_logdir
     overwrite = cli.overwrite
     sampling_sizes = cli.sampling_sizes
+    tar_path = cli.full_tar_path
 
     try:
         # Beware, sensitive to file structure
@@ -189,6 +195,16 @@ def main():
             raise FileNotFoundError("Could not find compute_shaps_template.sh")
     except IndexError as exc:
         raise FileNotFoundError("Could not find compute_shaps_template.sh") from exc
+
+    # Define hdf5 suffix with tar content
+    with tarfile.open(tar_path, "r") as tar:
+        tar_content = tar.getnames()
+        all_hdf5 = [Path(x) for x in tar_content if x.endswith(".hdf5")]
+        all_hdf5_suffix = [x.stem.split(sep="_", maxsplit=1)[1] for x in all_hdf5]
+    if len(set(all_hdf5_suffix)) != 1:
+        raise ValueError(f"Multiple hdf5 suffixes found: {all_hdf5_suffix}")
+    hdf5_suffix = all_hdf5_suffix[0]
+    print(f"Using hdf5 suffix for list creation: {hdf5_suffix}")
 
     # Find all split folders
     split_folders = [
@@ -241,7 +257,7 @@ def main():
         write_hdf5_paths_to_file(
             md5s=sorted(best_background_md5s),
             parent=".",
-            suffix="100kb_all_none",
+            suffix=hdf5_suffix,
             filepath=background_filename,
         )
 
@@ -257,7 +273,7 @@ def main():
         write_hdf5_paths_to_file(
             md5s=sorted(valid_md5),
             parent=".",
-            suffix="100kb_all_none",
+            suffix=hdf5_suffix,
             filepath=eval_filename,
         )
 
@@ -287,6 +303,7 @@ def main():
             ":::output_log:::": str(shap_folder),
             ":::background_list:::": str(background_filename),
             ":::eval_list:::": str(eval_filename),
+            ":::full_tar_path:::": str(tar_path),
         }
 
         # Read / replace / write
@@ -322,9 +339,10 @@ def main():
     print(
         """
         Don't forget to check for:
-        1) correct paths for where the scripts are run from (e.g. $HOME)
-        2) correct model paths (in best_checkpoint.list files)
-        3) Add analysis job to submit script if desired.
+        - Correct paths for where the scripts are run from (e.g. $HOME)
+        - Correct model paths (in best_checkpoint.list files)
+        - Correct hdf5 filenames in created lists.
+        Add analysis job to submit script if desired.
         """
     )
 

commit 463aef5786b7acbbc5a18bc1f45a67ad5691f2ec

Fix hdf5_parent failure. Enrich prints. - Fixes bug introduced in 7b3e52b59a066f9417ba6d2760e4e84304631256   dynamic hdf5_parent creation failed because of venv folder existence. - Add more timestamp/prints to bash script.

src/bash_utils/compute_shaps_template.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=8:00:00
 #SBATCH --account=:::account:::
 #SBATCH --job-name=:::job_name:::
 #SBATCH --output=/lustre06/project/6007017/rabyj/epilap/output/sub/slurm_files/%x-job%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=30
-#SBATCH --mem=128G
+#SBATCH --mem=80G
 #SBATCH --mail-user=:::email:::
 #SBATCH --mail-type=END,FAIL
 # shellcheck disable=SC2317  # Don't warn about unreachable commands in this file
 # shellcheck disable=SC2086  # Don't warn about double quoting
 # shellcheck disable=SC1091  # Don't warn about sourcing unreachable files
+
+echo "$(date +%F_%T) - Starting script"
+
+ml StdEnv/2020 python/3.8
 
 export PYTHONUNBUFFERED=TRUE
 
@@ -30,6 +34,7 @@ program_path="${gen_program_path}/src/python/epi_ml"
 slurm_out_folder="${gen_path}/epilap/output/sub/slurm_files"
 
 # use correct environment
+echo "$(date +%F_%T) - Loading and/or creating the virtual environment"
 set -e
 if [[ -n "$SLURM_JOB_ID" ]];
 then
@@ -64,7 +69,7 @@ chroms="${input_path}/chromsizes/hg38.noy.chrom.sizes"
 out1="${output_log}/output_job${SLURM_JOB_ID}_${SLURM_JOB_NAME}_${timestamp}.o"
 out2="${output_log}/output_job${SLURM_JOB_ID}_${SLURM_JOB_NAME}_${timestamp}.e"
 
-
+echo "$(date +%F_%T) - Evaluating paths."
 for path in ${list_background} ${list_explain} ${chroms}; do
   if [ ! -f ${path} ]; then
     echo "${path} is not a file. Please check the path."
@@ -75,6 +80,7 @@ for path in ${list_background} ${list_explain} ${chroms}; do
 done
 
 # --- Pre-checks ---
+echo "$(date +%F_%T) - Executing pre-checks"
 set -e # in case check_dir fails, to stop bash script
 program_path="${gen_path}/sources/epi_ml/src/python/epi_ml"
 cd ${program_path}
@@ -85,10 +91,6 @@ python ${program_path}/utils/check_dir.py --exists ${model_path}
 
 printf '%s\n' "python ${program_path}/utils/check_dir.py ${output_log}"
 python ${program_path}/utils/check_dir.py ${output_log}
-
-printf '\n%s\n' "Launching following command"
-printf '%s\n' "python ${program_path}/utils/preconditions.py"
-python ${program_path}/utils/preconditions.py
 
 # Preconditions passed, copy launch script to log dir.
 if [[ -n "$SLURM_JOB_ID" ]];
@@ -101,6 +103,7 @@ fi
 
 if [[ -n "$SLURM_JOB_ID" ]];
 then
+  echo "$(date +%F_%T) - Extracting hdf5s"
   tar_file=":::full_tar_path:::"  # IMPORTANT
 
   cd $SLURM_TMPDIR
@@ -108,7 +111,8 @@ then
   echo "Untaring $tar_file in $SLURM_TMPDIR"
   tar -xf $tar_file
 
-  folder_name=$(find . -mindepth 1 -type d | head -n 1)
+  # Finding correct hdf5 folder name
+  folder_name=$(find . -mindepth 1 -type d | grep -v "epiclass" | head -n 1)
   folder_name=$(basename $folder_name)
   echo "Using folder_name: $folder_name"
   export HDF5_PARENT="${folder_name}" # IMPORTANT
@@ -124,13 +128,11 @@ set +e
 basecmd="python ${program_path}/compute_shaps.py -m ${model} --background_hdf5 ${list_background} --explain_hdf5 ${list_explain} --chromsize ${chroms} -l ${output_log} -o explain_${category}"
 basecmd="${basecmd} --model_dir ${model_path}"
 
-
-echo "Time before launch: $(date +%F_%T)"
+echo "$(date +%F_%T) - Running main program"
 printf '\n%s\n' "Launching following command"
 printf '%s\n' "${basecmd} > ${out1} 2> ${out2}"
 ${basecmd} > ${out1} 2> ${out2}
-echo "Time after launch: $(date +%F_%T)"
-
+echo "$(date +%F_%T) - Time after main program"
 
 
 # Copy slurm output file to log dir
@@ -139,3 +141,5 @@ then
   slurm_out_file="${SLURM_JOB_NAME}-*${SLURM_JOB_ID}.out"
   cp -v ${slurm_out_folder}/${slurm_out_file} ${output_log}/
 fi
+
+echo "$(date +%F_%T) - END OF SCRIPT"