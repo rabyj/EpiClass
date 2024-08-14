rabyj/EpiClass

commit 52dff1a573ca840c5a9ccc428556416828c3f7a6

Fix hdf5 resolution retrieval.

src/python/epi_ml/core/data_source.py
@@ -54,12 +54,15 @@ class EpiDataSource:
         with open(self.hdf5_file, "r", encoding="utf-8") as my_file:
             first_path = Path(next(my_file).rstrip())
             try:
-                resolution = EpiDataSource.get_file_hdf5_resolution(first_path)
-            except FileNotFoundError:
-                warnings.warn(
-                    f"File not found: {first_path}. Seeking resolution with filename."
-                )
-                resolution = self.get_resolution_from_filename(first_path)
+                resolution = self.get_file_hdf5_resolution(first_path)
+            except KeyError as err:
+                warnings.warn(f"{err}. Seeking resolution from filename.")
+                try:
+                    resolution = self.get_resolution_from_filename(first_path)
+                except KeyError as err2:
+                    raise KeyError(
+                        f"Filename does not contain resolution: {first_path}"
+                    ) from err2
         return resolution
 
     @staticmethod

commit 57839d9c8f425f21ffe9c1363a7e9fd72f3a148a

Fix bug introduced in 52dff1a.
Had removed FileNotFoundError exception handling in data_source.py. I put it back.

src/python/epi_ml/core/data_source.py
@@ -55,7 +55,7 @@ class EpiDataSource:
             first_path = Path(next(my_file).rstrip())
             try:
                 resolution = self.get_file_hdf5_resolution(first_path)
-            except KeyError as err:
+            except (KeyError, FileNotFoundError) as err:
                 warnings.warn(f"{err}. Seeking resolution from filename.")
                 try:
                     resolution = self.get_resolution_from_filename(first_path)