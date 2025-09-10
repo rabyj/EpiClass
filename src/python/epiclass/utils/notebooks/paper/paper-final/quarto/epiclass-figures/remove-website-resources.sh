fd63a071e6ec38aea393408bf645b38f19da0dcd
rabyj <Joanny.Raby@usherbrooke.ca>
2025-09-10 17:44:12 -0400

    Add some website cleanup

---
+++ src/python/epiclass/utils/notebooks/paper/paper-final/quarto/epiclass-figures/remove-website-resources.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Remove unnecessary website resources
+# We can do this because we used the embed-resources option in quarto.
+rm -rf ../../../../../../../../../docs/epiclass-paper/resources
